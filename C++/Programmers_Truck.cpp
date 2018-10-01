58ac8440f436b04ba6c196f0e4f7227a447ec508
Neo_AlBan <albaneo0724@gmail.com>
2018-10-02 03:33:33 +0900

    ..

--- C++/Programmers_Truck.cpp
+++ C++/Programmers_Truck.cpp
@@ -1,7 +1,6 @@
-#include <string>
 #include <vector>
-#include <queue>
 #include <iostream>
+#include <queue>
 #include <chrono>
 
 using namespace std;
@@ -9,34 +8,65 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     int answer = 0;
-
     //EX 1)
     int bridge_length = 2;
     int weight = 10;
-    int weightSum = 0;
     vector<int> truck_weights = {7, 4, 5, 6};
-    vector<int> wait_truck;
-    queue<int> britdge;
 
-    //코드 성능 체크
+    queue<int> arr;
+    queue<int> bQue;
+    vector<int>::iterator itr = truck_weights.begin();
+    int cross_time = 1;
+    int weight_sum = 0;
+    int tmpidx = 0;
     auto start = chrono::high_resolution_clock::now();
-    //reverse와 같음   (뒤집어 복사)
-    wait_truck.assign(truck_weights.rbegin(), truck_weights.rend());
 
-    for (int i = 0; i < truck_weights.size(); i++)
-    {        
-        if(weightSum > weight){ //다리 무게 초과
+    while (itr != truck_weights.end())
+    {
 
-        }else{  //다리 무게 감소
-
+        if (weight >= weight_sum + *itr)
+        {
+            weight_sum += *itr;
+            if (bQue.size() < bridge_length)
+            {
+                bQue.push(*itr);
+                arr.push(cross_time);
+                cout << "the truck is in the bridge : " << *itr << endl;
+            }
+            else
+            {
+                int tmp = bQue.front();
+                weight_sum -= tmp;
+                cout << "Crossed over truck : " << tmp << endl;
+                bQue.pop();
+                if (bQue.empty())
+                {
+                    break;
+                }
+            }
+            cout << tmpidx << "번 째 Q 사이즈 : " << bQue.size() << endl;
+            tmpidx++;
         }
-        answer++;
+        else
+        {
+            if ((arr.front() + bridge_length) == cross_time)
+            {
+                weight_sum = 0;
+                cout << "Crossed over truck : " << bQue.front() << endl;
+                bQue.pop();
+                arr.pop();
+            }
+            cross_time++;
+            continue;
+        }
+        cross_time++;
+        itr++;
     }
+    answer = cross_time;
 
-
-
+    //프로그램 실행시간 체크
     auto end = chrono::high_resolution_clock::now();
-    cout << "Time : " << (float)(end - start).count() / CLOCKS_PER_SEC << endl;
+    cout << "Duration Time : " << (float)(end - start).count() / CLOCKS_PER_SEC << " sec" << endl;
     cout << "Answer : " << answer << endl;
     return 0;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,13 +1,20 @@
+#include <vector>
 #include <iostream>
+
 using namespace std;
 
-int main()
+int main(int argc, char const *argv[])
 {
-    string arrangement = "()(((()())(())()))(())";
-    int answer = 0;
+    vector < vector <int> > vec(4, vector<int>(4,0));
 
-    auto itr = arrangement.begin();
-
+    
+    int i =0;
+    while(!vec.empty()){
+        vec.pop_back();
+        i++;
+        cout<<i<<endl;
+    }
+    
     
     return 0;
 }
