cf872e336e0b9d2e3567038519ce53373fc01805
Neo_AlBan <albaneo0724@gmail.com>
2018-10-12 21:13:44 +0900

    a

--- C++/Programmers_Shortest_Distance.cpp
+++ C++/Programmers_Shortest_Distance.cpp
@@ -8,21 +8,13 @@ int findRoad(vector<vector<int>> maps, int xpos, int ypos, int xLen, int yLen);
 
 int main(int argc, char const *argv[])
 {
-    vector<vector<int>> maps1 = {{1, 0, 1, 1, 1},
-                                 {1, 0, 1, 0, 1},
-                                 {1, 0, 1, 1, 1},
-                                 {1, 1, 1, 0, 1},
-                                 {0, 0, 0, 0, 1}};
+    vector<vector<int>> maps1 = {{1, 0, 1, 1, 1}, {1, 0, 1, 0, 1}, {1, 0, 1, 1, 1}, {1, 1, 1, 0, 1}, {0, 0, 0, 0, 1}};
 
-    vector<vector<int>> maps2 = {{1, 0, 1, 1, 1},
-                                 {1, 0, 1, 0, 1},
-                                 {1, 0, 1, 1, 1},
-                                 {1, 1, 1, 0, 0},
-                                 {0, 0, 0, 0, 1}};
+    vector<vector<int>> maps2 = {{1, 0, 1, 1, 1}, {1, 0, 1, 0, 1}, {1, 0, 1, 1, 1}, {1, 1, 1, 0, 0}, {0, 0, 0, 0, 1}};
 
     // int result1 = solution(maps1);
-    int result2 = solution(maps2);
     // cout << "Result 1 : "<< result1 << endl;
+    int result2 = solution(maps2);
     cout << "Result 2 : " << result2 << endl;
     return 0;
 }
@@ -49,33 +41,33 @@ int solution(vector<vector<int>> maps)
             {
                 //Right
                 cout << "Move Down" << endl;
+                cnt++;
                 maps[xpos][ypos] = 0;
                 xpos++;
-                cnt++;
             }
             else if (maps[xpos][ypos + 1] == 1)
             {
                 //Down
                 cout << "Move Right" << endl;
+                cnt++;
                 maps[xpos][ypos] = 0;
                 ypos++;
-                cnt++;
             }
             else if (maps[xpos - 1][ypos] == 1)
             {
                 //Up
                 cout << "Move Up" << endl;
+                cnt++;
                 maps[xpos][ypos] = 0;
                 xpos--;
-                cnt++;
             }
             else if (maps[xpos][ypos - 1] == 1)
             {
                 //Left
                 cout << "Move Left" << endl;
+                cnt++;
                 maps[xpos][ypos] = 0;
                 ypos--;
-                cnt++;
             }
             else
             {
