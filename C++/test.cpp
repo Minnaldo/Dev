7b8b5871cce48e400b806f155aa5a579acec930d
username <user@email.mail>
2018-10-15 00:21:57 +0900

    zz

--- C++/test.cpp
+++ C++/test.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include <vector>
 #include <algorithm>
 #include <iostream>
@@ -31,14 +32,47 @@ vector<int> solution(vector<int> array, vector<vector<int>> commands)
         cout << answer.at(i) << "  ";
     }
     cout << endl;
+=======
+#include<iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+    
+    for (int i = 0; i < commands.size(); i++)
+    {
+            auto itr = array.begin() + (commands[i][0] - 1);
+            auto itr2 = array.begin() + (commands[i][1] - 1);
+            vector<int> tmp;
+            tmp.assign(itr, itr2);
+            sort(tmp.begin(), tmp.end());
+            answer.push_back(*(tmp.begin() + commands[i][2] - 1));     
+    }
+>>>>>>> ad4b6da2c458c163bacec1f6e6dc7950f0a483fa
     return answer;
 }
 
 int main(int argc, char const *argv[])
 {
+<<<<<<< HEAD
     vector<int> array = {1, 5, 2, 6, 3, 7, 4};
     vector<vector<int>> commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
 
     solution(array, commands);
+=======
+    vector <int> arr = {1, 5, 2, 6, 3, 7, 4};
+    vector < vector< int>> commands = { {2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+        vector <int> tmpAnswer = solution(arr, commands);
+
+    
+    for(int i = 0; i < tmpAnswer.size(); i++)
+    {
+        cout<<tmpAnswer[i]<<" "<<endl;
+    }
+    
+>>>>>>> ad4b6da2c458c163bacec1f6e6dc7950f0a483fa
     return 0;
 }
