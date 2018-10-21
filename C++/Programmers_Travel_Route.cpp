c1be6dcf83ddfada9006ae102e1e6ec25af02c23
Neo_AlBan <albaneo0724@gmail.com>
2018-10-21 17:55:07 +0900

    aa

--- C++/Programmers_Travel_Route.cpp
+++ C++/Programmers_Travel_Route.cpp
@@ -8,6 +8,7 @@ using namespace std;
 /**
  * * 여행경롤를 탐색하는 알고리즘
  * * DFS를 이용하자 (인접 리스트 DFS)
+ * * BFS도 이용 가능할 듯
  * * LG CNS Code Monster와 비슷하다
  *  */
 
@@ -18,38 +19,49 @@ int ticketQuantity;      //처음 들어오는 티켓의 갯수를 저장하기 
 
 void dfs(string arrive, vector<vector<string>> ticket)
 {
-    s.push(arrive);
-
-    // string current = s.top();
+    stopOver.push_back(arrive);
     string current = arrive;
     for (int i = 0; i < ticketQuantity; i++)
     {
         if (!visit[i] && current == ticket[i][0])
         {
-            // s.push(ticket[i][0]);
-            stopOver.push_back(ticket[i][0]);
-            stopOver.push_back(ticket[i][1]);
             visit[i] = true;
-            dfs(ticket[i][0], ticket);
+            dfs(ticket[i][1], ticket);
         }
     }
 }
 
 vector<string> solution(vector<vector<string>> tickets)
 {
-    // visit = new bool[ticketQuantity];
     ticketQuantity = tickets.size();
-    // s.push(tickets[0][0]); // push ICN (push department)
-    stopOver.push_back(tickets[0][0]);
-    stopOver.push_back(tickets[0][1]);
-    visit[0] = true;
+    string department;
+    int tmpIdx;
 
-    for (int i = 0; i < ticketQuantity; i++)
+    for(int i =0; i<ticketQuantity; i++)
     {
-        if (!visit[i])
+        for(int j = i; j<ticketQuantity; i++)
         {
-            dfs(tickets[i][1], tickets);
+            if(tickets[i][0] == tickets[j][0] )
+            {
+                if(tickets[i][0].compare(tickets[j][0]) < 0 )
+                {
+                    department = tickets[i][0];
+                    tmpIdx = i;
+                }
+                else
+                {
+                    department = tickets[j][0];
+                    tmpIdx = j;
+                }
+            }
         }
+    }
+
+    stopOver.push_back(department);
+    if(!visit[tmpIdx])
+    {
+        visit[tmpIdx] = true;
+        dfs(tickets[tmpIdx][1], tickets);
     }
 
     return stopOver;
@@ -63,7 +75,7 @@ int main(int argc, char const *argv[])
 
     for (int i = 0; i < answer.size(); i++)
     {
-        cout << answer[i];
+        cout << answer[i] << " ";
     }
     cout << endl;
 
