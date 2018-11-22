57831765a3573182d20cb4b96056e3f8453687d9
username <user@email.mail>
2018-11-22 16:15:08 +0900

    Dynamic Programming 1_back1463

--- C++/Back_1463.cpp
+++ C++/Back_1463.cpp
@@ -3,8 +3,15 @@
 
 using namespace std;
 
+/**
+ * * 문제와 반대로 1->N을 만들수 있는 경우의 수를 찾아 최소연산을 리턴
+ */
+
+//메모 배열에 연산 횟수를 더한다
 int memo[1000001];
 
+// parameter 두개를 입력받아 작은 걸 반환
+// 이문제에서는 더 작은 연산횟수를 반환하기 위함
 int min(int a, int b)
 {
     return a > b ? b : a;
@@ -15,18 +22,19 @@ int main(int argc, char const *argv[])
     int N;
     cin >> N;
 
+    // 1부터 시작하기 위해 memo[1]을 시작점으로
     memo[1] = 0;
 
     for (int i = 2; i <= N; i++)
     {
-        memo[i] = memo[i - 1] + 1;
+        memo[i] = memo[i - 1] + 1; // memo[i-1] ==> 의 이유 if i가 memo[i-1]에서 1을 더해 나올 수 있는 수 이므로 연산을 하고 연산횟수 1을 더해준다
         if (i % 2 == 0)
         {
-            memo[i] = min(memo[i], memo[i / 2] + 1);
+            memo[i] = min(memo[i], memo[i / 2] + 1); // memo[i/2] ==> 의 이유 if i가 2의 배수 일때 i/2에서 2를 곱해 나올수 있는 수 이므로
         }
         if (i % 3 == 0)
         {
-            memo[i] = min(memo[i], memo[i / 3] + 1);
+            memo[i] = min(memo[i], memo[i / 3] + 1); // memo[i/3] ==> 의 이유 if i가 3의 배수 일때 i/3에서 3를 곱해 나올수 있는 수 이므로
         }
     }
 
