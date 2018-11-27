16aa045cfd686a1caf24a757cc7ff6a4c37787a6
Neo_AlBan <albaneo0724@gmail.com>
2018-11-27 22:07:47 +0900

    commit

---
+++ C++/back_2193.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+/** 이친수 - 백준_2193 (https://www.acmicpc.net/problem/2193)
+ *  * 방사형태로 퍼져나가며 결정하는 문제 ( <=> 한가지 결과로 수렴해가는 문제 )
+ */
+
+using namespace std;
+
+int dp[90], cnt;
+
+int solution(int n, int T)
+{
+
+}
+
+int main(int argc, char const *argv[])
+{
+    int N = 3;
+    // cin >> N;
+    solution(1, N - 1);
+    cout << cnt << endl;
+    return 0;
+}
