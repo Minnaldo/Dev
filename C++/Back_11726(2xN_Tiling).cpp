51bb59a4bba7eb9f6e8014356db922277a66ce2f
Neo_AlBan <albaneo0724@gmail.com>
2018-11-29 01:10:06 +0900

    2xN Tiling update

--- C++/Back_11726(2xN_Tiling).cpp
+++ C++/Back_11726(2xN_Tiling).cpp
@@ -3,6 +3,9 @@
 /** 2 x n 타일링 백준_11726 (https://www.acmicpc.net/problem/11726)
  *  * Success, 자료형의 크기로 인한 음수값이 나오는 경우 생김 ex) n = 92
  *  ! 다른사람의 설명을 봐서 추가적인 공부가 필요할듯 NOTE
+ *  * 1) dp[n-1]에서 1x2 사각형(세로)를 붙여 주었을때의 수
+ *  * 2) dp[n-2]에서 2x2(가로 사각형 2개)를 붙여 주었을때의 수
+ *  * 1, 2를 더하면 현재(dp[n])에서의 경우의 수가 된다.
  */
 using namespace std;
 
@@ -15,7 +18,7 @@ long long solution(int n)
         dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
     }
 
-    return dp[n] % 10007;
+    return dp[n];
 }
 
 int main(int argc, char const *argv[])
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string tmp;
+    cin>>tmp;
+    cout<<tmp<<endl;
+    return 0;
+}
