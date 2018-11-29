20ce6b98cfb1a8190f3fa32c5560614ef12e3388
username <user@email.mail>
2018-11-29 18:46:02 +0900

    commit

--- C++/back_1912.cpp
+++ C++/back_1912.cpp
@@ -2,7 +2,7 @@
 #include <sstream>
 
 /** 연속합 백준_1912 (https://www.acmicpc.net/problem/1912)
- *
+ *  * 음수를 더하여도 최대가 될 수있는 경우가 생김
  */
 
 using namespace std;
@@ -18,8 +18,17 @@ int solution(int n, int *arr)
 {
     for (int i = 1; i <= n; i++)
     {
-        dp[i] = max(dp[i - 1], dp[i - 1] + arr[i]);
-        dp[i] = max(dp[i], arr[i]);
+        // if(dp[i-1] > (dp[i-1] + arr[i]))
+        // {
+        //     dp[i] = max(arr[i], dp[i] +arr[i]);
+        // }
+        // else
+        // {
+        //     dp[i] = dp[i-1]+arr[i];
+        // }
+
+        dp[i] = max(dp[i-1], dp[i-1]+arr[i]);
+        // dp[i] = max()
         cout << dp[i] << " ";
     }
     cout << endl;
