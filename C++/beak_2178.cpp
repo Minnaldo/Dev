2145ca1e5d58f4446b5bf70203bec16185409560
Neo_AlBan <albaneo0724@gmail.com>
2018-12-04 16:58:51 +0900

    beak 2178 completion

--- C++/beak_2178.cpp
+++ C++/beak_2178.cpp
@@ -1,31 +1,30 @@
 #include <iostream>
-#include <cstdio>
 #include <queue>
 #include <algorithm>
+#include <sstream>
 
 /** 미로 탐색 백준_2178 (https://www.acmicpc.net/problem/2178)
  *  TODO 입력값을 나눠받아야 함
  */
 
-
 using namespace std;
 
 bool visit[100][100];
-int cnt[100][100], dir[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+int cnt[100][100];
+int dir[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 queue<pair<int, int>> q;
 
-int min(int a, int b)
+int solution(string *arr, int n, int m)
 {
-    return a < b ? a : b;
-}
-
-int solution(int **arr, int n, int m)
-{
-    int cur_xpos = q.front().second;
-    int cur_ypos = q.front().first;
-
+    // 시작 좌표
+    q.push(make_pair(0, 0));
+    visit[0][0] = true;
+    cnt[0][0] = 1;
+    int cur_ypos, cur_xpos;
     while (!q.empty())
     {
+        cur_ypos = q.front().first;
+        cur_xpos = q.front().second;
         for (int i = 0; i < 4; i++)
         {
             int nxt_ypos = cur_ypos + dir[i][0];
@@ -33,43 +32,53 @@ int solution(int **arr, int n, int m)
 
             if (nxt_ypos >= 0 && nxt_ypos < n && nxt_xpos >= 0 && nxt_xpos < m)
             {
-                if (!visit[nxt_ypos][nxt_xpos] && (arr[nxt_ypos][nxt_xpos] == 1))
+                if (!visit[nxt_ypos][nxt_xpos] && ((arr[nxt_ypos][nxt_xpos] - '0') > 0))
                 {
                     q.push(make_pair(nxt_ypos, nxt_xpos));
+                    cnt[nxt_ypos][nxt_xpos] = cnt[cur_ypos][cur_xpos] + 1;
                     visit[nxt_ypos][nxt_xpos] = true;
-
-                    if (cnt[cur_ypos][cur_xpos] > 1)
-                    {
-                        cnt[nxt_ypos][nxt_xpos] = min(cnt[nxt_ypos][nxt_xpos], cnt[cur_ypos][cur_xpos] + 1);
-                    }
                 }
             }
         }
         q.pop();
     }
 
-    return cnt[n][m];
+    return cnt[n-1][m-1];
 }
 
 int main(int argc, char const *argv[])
 {
+    string str = "4 6 101111 101010 101011 111011";
+    string str2 = "2 25 1011101110111011101110111 1110111011101110111011101";
+    stringstream ss;
+    ss.str(str);
     int N, M;
-    scanf("%d %d", &N, &M);
+    ss >> N >> M;
 
-    int **arr = new int *[N];
+    // (0,0) 부터 시작한다
+    string *arr = new string[N];
     for (int i = 0; i < N; i++)
     {
-        arr[i] = new int[M];
-        for (int j = 0; j < M; j++)
-        {
-            scanf("%1d", &arr[i][j]);
-        }
+        string tmp;
+        ss >> tmp;
+        arr[i] = tmp;
     }
 
-    q.push(make_pair(0, 0));
-    visit[0][0] = true;
-    cnt[0][0] = 1;
+    // for (int i = 1; i <= N; i++)
+    // {
+    //     for (int j = 0; j < M; j++)
+    //     {
+    //         cout << arr[i][j] << " ";
+    //         if((arr[i][j]-'0') == 1)
+    //         {
+    //             cout<< "this is integer"<<endl;
+    //         }
+    //     }
+    //     cout << endl;
+    // }
 
-    printf("%d\n", solution(arr, N, M));
+    cout << "Answer : " << solution(arr, N, M) << endl;
+    delete[] arr;
+
     return 0;
 }
