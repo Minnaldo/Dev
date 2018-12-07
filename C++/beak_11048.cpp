9b5ec917d2bbb5c3d4fe3bb4c934dd4ebc5505b9
Neo_AlBan <albaneo0724@gmail.com>
2018-12-08 00:32:56 +0900

    Update beak_11048.cpp

--- C++/beak_11048.cpp
+++ C++/beak_11048.cpp
@@ -1,28 +1,27 @@
 #include <iostream>
-#include <algorithm>
 #include <sstream>
 
 using namespace std;
 
-/** 이동하기 백준_11048 (https://www.acmicpc.net/problem/11048)
- *  ? 반례를 찾자 -> 예제는 다 맞으나, 정답이 아님
- */
-
 int dp[1001][1001];
 int dir[3][2] = {{-1, 0}, {0, -1}, {-1, -1}};
 
+int max(int a, int b)
+{
+    return a > b ? a : b;
+}
+
 int solution(int **arr, int n, int m)
 {
-    // y좌표
-    for (int i = 2; i <= n; i++)
+    for (int i = 1; i <= n; i++)
     {
-        // x좌표
-        for (int j = 2; j <= m; j++)
+        for (int j = 1; j <= m; j++)
         {
             for (int k = 0; k < 3; k++)
             {
                 int nxt_y = i + dir[k][0];
                 int nxt_x = j + dir[k][1];
+
                 if (nxt_y >= 1 && nxt_y <= n && nxt_x >= 1 && nxt_x <= m)
                 {
                     dp[i][j] = max(arr[i][j] + dp[nxt_y][nxt_x], dp[i][j]);
@@ -36,12 +35,10 @@ int solution(int **arr, int n, int m)
 
 int main(int argc, char const *argv[])
 {
-    string str = "3 4 1 2 3 4 0 0 0 5 9 8 7 6";     //31
-    string str2 = "3 3 1 0 0 0 1 0 0 0 1";          //3
-    string str3 = "4 3 1 2 3 6 5 4 7 8 9 12 11 10"; //47
+    string str  = "3 4 1 2 3 4 0 0 0 5 9 8 7 6";    //31
+    string str2 = "1 2 1 1";    //2
     stringstream ss;
-    ss.str(str2);
-
+    ss.str(str);
     int n, m;
     ss >> n >> m;
 
@@ -58,13 +55,6 @@ int main(int argc, char const *argv[])
     }
 
     dp[1][1] = arr[1][1];
-
-    for (int i = 2; i <= n; i++)
-    {
-        dp[i][1] = max(dp[i][1], dp[i - 1][1] + arr[i][1]);
-        dp[1][i] = max(dp[1][i], dp[1][i - 1] + arr[1][i]);
-    }
-
     cout << solution(arr, n, m) << endl;
 
     return 0;
