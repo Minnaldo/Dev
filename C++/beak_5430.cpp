7da9a051a249da096f3961cb7b3ba8944698da08
Neo_AlBan <albaneo0724@gmail.com>
2018-12-11 00:37:42 +0900

    aa

---
+++ C++/beak_5430.cpp
@@ -0,0 +1,78 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, int n, string func)
+{
+    auto itr = arr.begin();
+    vector<int> arr2(n, 0);
+    auto strItr = func.begin();
+    int idx = 0;
+    while (func[idx] != ' ')
+    {
+        if (func[idx] == 'D')
+        {
+            if (itr == arr.end())
+            {
+                cout << "error" << endl;
+                exit(1);
+            }
+            else
+            {
+                *itr = 0;
+            }
+        }
+        else if (func[idx] == 'R')
+        {
+            auto ritr = arr.rbegin();
+            for (int i = 0; i < arr2.size(); i++)
+            {
+                arr2[i] = *ritr;
+                ritr++;
+            }
+        }
+        idx++;
+    }
+
+    return arr2;
+}
+
+int main(int argc, char const *argv[])
+{
+    ifstream fs("input.txt");
+    fs
+    int T, n;
+    cin >> T;
+
+    for (int i = 0; i < T; i++)
+    {
+        string func;
+        cin >> func; //수행할 케이스
+        cin >> n;    //배열 크기, 숫자 갯수
+        vector<int> arr(n, 0);
+        string array; // 숫자들
+        getline(cin, array, ',');
+        for (int j = 0; j < n; j++)
+        {
+            int tmp;
+            cin >> tmp;
+            arr[j] = tmp;
+        }
+
+        vector<int> answer = solution(arr, n, func);
+        cout << "[";
+        for (int k = 0; k < answer.size(); k++)
+        {
+            cout << answer[k];
+            if (k != answer.size())
+            {
+                cout << ",";
+            }
+        }
+        cout << "]" << endl;
+    }
+    return 0;
+}
