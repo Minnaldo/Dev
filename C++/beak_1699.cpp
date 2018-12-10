dfbcc255319d3495064c1eb4946d135004075733
username <user@email.mail>
2018-12-10 18:35:07 +0900

    aa

--- C++/beak_1699.cpp
+++ C++/beak_1699.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 /** 백준 제곱수_1699 (https://www.acmicpc.net/problem/1699)
  *  TODO 반례체크
+ *  점화식 : dp[n] = n^2로 T를 만들때 필요한 n^2의 갯수
+ *  * 동전 교환 알고리즘을 이용하면 풀 수 있을까?
  */
 
 int dp[100001];
@@ -13,28 +15,33 @@ int min(int a, int b)
     return a < b ? a : b;
 }
 
-int solution(int n)
+int solution(int n, int *arr)
 {
     for (int i = 1; i <= n; i++)
     {
-        for (int j = 1; j*j <= n; j++)
+        for (int j = arr[i]; j <= n; j++)
         {
-            int tmp = i - (j * j);
-            if (tmp >= 0)
-            {
-                dp[i] = min(dp[tmp] + 1, dp[i - 1] + 1);
-            }
+            dp[i] = min(dp[i - j] + 1, dp[j]);
         }
+        cout << dp[i] << endl;
     }
     return dp[n];
 }
 
 int main(int argc, char const *argv[])
 {
+    ios::sync_with_stdio(false);
     int T;
-    cin >> T;   // 142 --> 3
+    cin >> T; // 142 --> 3
     // int T = 142;
-
-    cout << solution(T) << endl;
+    // int T = 43; // 3
+    int *arr = new int[T + 1];
+    //전처리
+    for (int i = 1; i <= T; i++)
+    {
+        dp[i] = i;
+        arr[i] = i * i;
+    }
+    cout << solution(T, arr) << endl;
     return 0;
 }
--- C++/beak_1912.cpp
+++ C++/beak_1912.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <sstream>
 
+using namespace std;
+
 /** 연속합 백준_1912 (https://www.acmicpc.net/problem/1912)
  *  * 음수를 더하여도 최대가 될 수있는 경우가 생김
+ *  * 점화식 dp[n] = n에서 나올 수 있는 연속합 중 최댓값
+ *  ! dp[n-1] + arr[n] < 0 && dp[n-1] < 0 일 때, arr[n]을 더하는 것은 의미가 없다 ==> dp[m] = arr[n]
+ *  ! 이 외의 경우 기본적인 점화식은 dp[n] = dp[n-1] + arr[n]이 성립한다
  */
-
-using namespace std;
 
 int dp[100001];
 
@@ -13,24 +16,31 @@ int max(int a, int b)
 {
     return a > b ? a : b;
 }
-
 int solution(int n, int *arr)
 {
+    int ans = dp[1];
     for (int i = 2; i <= n; i++)
     {
-        dp[i] = max(arr[i], dp[i-1] + arr[i]);
-        cout << dp[i] << " ";
+        if (dp[i - 1] + arr[i] > 0 && dp[i - 1] > 0)
+        {
+            dp[i] = dp[i - 1] + arr[i];
+        }
+        else
+        {
+            dp[i] = arr[i];
+        }
+
+        ans = max(ans, dp[i]);
     }
-    cout << endl;
-    return max(dp[n], dp[n - 1]);
+    return ans;
 }
 
 int main(int argc, char const *argv[])
 {
-    string str = "10 10 -4 3 1 5 6 -35 12 21 -1";
-    string str2 = "5 -1 0 5 -3 4";  //correct : 5
+    string str = "10 10 -4 3 1 5 6 -35 12 21 -1"; //33
+    string str2 = "5 -1 0 5 -3 4";                // 5
     stringstream ss;
-    ss.str(str2);
+    ss.str(str);
 
     int T;
     ss >> T;
@@ -42,7 +52,7 @@ int main(int argc, char const *argv[])
         ss >> tmp;
         arr[i] = tmp;
     }
-    dp[1] = max(dp[0], dp[0] + arr[1]);
+    dp[1] = arr[1];
     cout << solution(T, arr) << endl;
     return 0;
 }
