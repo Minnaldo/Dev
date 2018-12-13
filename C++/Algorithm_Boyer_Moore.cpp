4704717c160eab83170d01377ec97d48e5fff7bd
username <user@email.mail>
2018-12-13 21:19:39 +0900

    commit

---
+++ C++/Algorithm_Boyer_Moore.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/** 보이어 무어 알고리즘 문자열 매칭 알고리즘
+ *
+ */
+
+void badCharacterShiftRule(string pattern)
+{
+    int pSize = pattern.size();
+
+    for(int i = pSize; i>0; i--)
+    {
+
+    }
+}
+
+void goodSuffixShiftRule()
+{
+
+}
+
+int main(int argc, char const *argv[])
+{
+
+    return 0;
+}
