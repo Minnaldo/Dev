3b8d18773e1a75d55869f63ae12b63a25a97aeaa
Neo_AlBan <albaneo0724@gmail.com>
2018-12-13 11:07:46 +0900

    commit

--- C++/Sock_C.cpp
+++ C++/Sock_C.cpp
@@ -14,46 +14,48 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    struct sockaddr_in serveraddr;
+    struct sockaddr_in server_addr;
     char buf[MAXLINE];
     // * 1번째 인자 - 프로토콜 체계 (PF_INET : IPv4, PF_INET6 : IPv6) 이들은 매크로 상수
     // * 2번째 인자 - 서비스 타입 (SOCK_STREAM : TCP, SOCK_DGRAM : UDP)
-    int sock = socket(PF_INET, SOCK_STREAM, 0);
-    if (sock < 0)
+    int client_socket = socket(PF_INET, SOCK_STREAM, 0); //클라이언트 소켓 설정
+    if (client_socket == -1)
     {
-        cerr << "Socket Failed";
+        cerr << "Socket Failed\n";
         return 1;
     }
 
-    serveraddr.sin_family = AF_INET;
-    serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    serveraddr.sin_port = htons(9999);
+    server_addr.sin_family = AF_INET;                     // 프로토콜 방식
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); //접속할 서버의 주소 설정
+    server_addr.sin_port = htons(9999);                   //접속할 서버의 포트번호
 
-    socklen_t client_len = sizeof(serveraddr);
+    socklen_t client_len = sizeof(server_addr);
 
-    if (connect(sock, (struct sockaddr *)&serveraddr, client_len) == -1)
+    if (connect(client_socket, (struct sockaddr *)&server_addr, client_len) == -1)
     {
-        cerr << "coonect error" << endl;
+        cerr << "coonect error\n";
         return 1;
     }
     memset(buf, 0x00, MAXLINE); //cstring헤더에 포함 : memset
 
-    if (send(sock, buf, MAXLINE, MSG_NOSIGNAL) <= 0)
+    //여기부터 while문을 통해 반복되게 만든다, 종료 조건은?
+    while (1)
     {
-        cerr << "write error";
-        return 1;
+        if (send(client_socket, buf, MAXLINE, MSG_NOSIGNAL) <= 0)
+        {
+            cerr << "write error\n";
+            return 1;
+        }
+
+        if (recv(client_socket, buf, MAXLINE, 0) <= 0)
+        {
+            cerr << "read error\n";
+            return 1;
+        }
+
+        cout << "server : " << buf << endl;
+        memset(buf, 0x00, MAXLINE); // 입력 버퍼 초기화
     }
-    memset(buf, 0x00, MAXLINE);
-
-    if (recv(sock, buf, MAXLINE, 0) <= 0)
-    {
-        perror("read error");
-        return 1;
-    }
-
-    // recv(sock, buf, MAXLINE, 0);
-    cout << "server : " << buf << endl;
-
-    shutdown(sock, SHUT_RDWR);
+    shutdown(client_socket, SHUT_RDWR);
     return 0;
 }
--- C++/Sock_S.cpp
+++ C++/Sock_S.cpp
@@ -17,7 +17,7 @@ int main(int argc, char const *argv[])
     int server_socket;
     int client_socket;
 
-    struct sockaddr_in serveraddr;
+    struct sockaddr_in server_addr;
     struct sockaddr_in client_addr;
 
     char buf_rcv[MAXLINE + 5];
@@ -26,46 +26,48 @@ int main(int argc, char const *argv[])
     server_socket = socket(PF_INET, SOCK_STREAM, 0);
     if (-1 == server_socket)
     {
-        cerr << "server socket 생성 실패" << endl;
+        cerr << "server socket 생성 실패\n";
         exit(1);
     }
 
-    memset(&serveraddr, 0, sizeof(serveraddr));
-    serveraddr.sin_family = AF_INET;
-    serveraddr.sin_port = htons(9999);
-    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;   // 프로토콜
+    server_addr.sin_port = htons(9999); // 열어둘 포트 번호
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    if (-1 == bind(server_socket, (struct sockaddr *)&serveraddr, sizeof(serveraddr)))
+    if (-1 == bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)))
     {
-        cerr << "bind() 실행 에러" << endl;
+        cerr << "binding error\n";
         exit(1);
     }
 
+    // 서버는 계속해서 확인하고 응답해야하므로 반복문을 통해 반복한다
     while (1)
     {
+        // 포트번호 열어두고 listen
         if (-1 == listen(server_socket, 5)) // 소켓을 수동 대기모드로 설정
         {
-            cerr << "대기상태 모드 설정 실패" << endl;
+            cerr << "대기상태 모드 설정 실패\n";
             exit(1);
         }
 
         socklen_t client_addr_size = sizeof(client_addr);
         client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &client_addr_size);
-
         if (-1 == client_socket)
         {
-            cerr << "클라이언트 연결 수락 실패" << endl;
+            cerr << "클라이언트 연결 수락 실패\n";
             exit(1);
         }
-        char temp[20];
+
+        char temp[20]; //접속한 클라이언트의 IP주소를 담을 배열
         inet_ntop(AF_INET, &client_addr.sin_addr.s_addr, temp, sizeof(temp));
-        cout<<"Server : "<<temp<<" client connected\n";
+        cout << "Server : " << temp << " client connected\n";
 
         recv(client_socket, buf_rcv, MAXLINE, 0);
-        cout << "receive : " << buf_rcv << endl;
-        cout << buf_snd << strlen(buf_rcv) << " : " << buf_rcv << endl;
+        cout << "receive : " << buf_rcv << "\n";
+        cout << buf_snd << strlen(buf_rcv) << " : " << buf_rcv << "\n";
         send(client_socket, buf_snd, strlen(buf_snd) + 1, 0); //NULL까지 전송
-        shutdown(client_socket, SHUT_RDWR);
+        shutdown(client_socket, SHUT_RDWR);                   //클라이언트와 연결된 소켓 제거
     }
     shutdown(server_socket, SHUT_RDWR);
 
