c05c452203bbc5bcbff886011b3ab453a400891c
Neo_AlBan <albaneo0724@gmail.com>
2018-12-20 03:32:51 +0900

    zz

--- C++/beak_1697.cpp
+++ C++/beak_1697.cpp
@@ -1,35 +1,45 @@
 #include <iostream>
 #include <queue>
-#include <algorithm>
 
 using namespace std;
 
-int dp[100001];
+/** 숨바꼭질 백준_1697 (https://www.acmicpc.net/problem/1697)
+ *  ! 메모리초과
+ *  REVIEW
+ */
+
+int dp[100000], cnt;
+
+int min(int a, int b)
+{
+    return a < b ? a : b;
+}
 
 int solution(int n, int k)
 {
     queue<int> q;
     q.push(n);
-    dp[n] = 1;
-    dp[k] = 987654321;
+    dp[n] = 0;
+
     while (!q.empty())
     {
         int cur_pos = q.front();
 
-        for (int i = 0; i < 3; i++)
+        while (cnt < 3)
         {
-            int nxt_pos = 0;
-            if (i == 0)
+            int nxt_pos;
+
+            switch (cnt)
             {
+            case 0:
                 nxt_pos = cur_pos + 1;
-            }
-            else if (i == 1)
-            {
+                break;
+            case 1:
                 nxt_pos = cur_pos - 1;
-            }
-            else if (i == 2)
-            {
+                break;
+            case 2:
                 nxt_pos = cur_pos * 2;
+                break;
             }
 
             if (nxt_pos >= 0 && nxt_pos <= 100000)
@@ -37,19 +47,33 @@ int solution(int n, int k)
                 q.push(nxt_pos);
                 dp[nxt_pos] = min(dp[cur_pos] + 1, dp[nxt_pos]);
             }
+            cnt++;
         }
+        cnt = 0;
+
         q.pop();
 
         if (q.front() == k)
         {
+            break;
         }
     }
+    return dp[k];
 }
 
 int main(int argc, char const *argv[])
 {
-    int S = 5, B = 17; // 4
+    ios::sync_with_stdio(false);
+    // int S = 5, B = 17; // 4
+    int S = 4, B = 100000;  //seg fault
+    // int S, B;
     // cin >> S >> B;
+
+    //초기값 100001의 이유 --> 아무리 오래걸려도 이시간 이내에는 도착할 것이다. == 이보다 큰 값은 나올 리 없다
+    for (int i = 0; i < 100000; i++)
+    {
+        dp[i] = 100001;
+    }
 
     cout << solution(S, B) << endl;
 
