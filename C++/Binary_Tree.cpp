ef0d9cab5a951bd04d9cdfd5ede00cdecb3b28b6
username <user@email.mail>
2018-12-25 14:02:26 +0900

    Binary Search Tree complete

--- C++/Binary_Tree.cpp
+++ C++/Binary_Tree.cpp
@@ -37,15 +37,76 @@ class Tree
         root = new TreeNode<T>(data);
     }
 
-    void buildTree()
+    TreeNode<T> *getRoot()
     {
-        root->left = new TreeNode<T>('B', new TreeNode<T>('D', new TreeNode<T>('H')), new TreeNode<T>('E', new TreeNode<T>('I'), new TreeNode<T>('J')));
-        root->right = new TreeNode<T>('C', new TreeNode<T>('F'), new TreeNode<T>('G', null, new TreeNode<T>('K')));
+        return root;
+    }
+
+    T getRootVal()
+    {
+        return root->data;
+    }
+
+    void insertNode(TreeNode<T> *node)
+    {
+        if (search(root, node->data) == NULL)
+        {
+            TreeNode<T> *parent = NULL;
+            TreeNode<T> *current = root;
+
+            while (current != NULL)
+            {
+                parent = current;
+                if (node->ata < parent->data)
+                {
+                    current = current->left;
+                }
+                else
+                {
+                    current = current->right;
+                }
+            }
+
+            if (node->data < parent->data)
+            {
+                parent->left = node;
+            }
+            else
+            {
+                parent->right = node;
+            }
+            cout << "Inserted " << node->data << endl;
+        }
+    }
+
+    void visit(TreeNode<T> *current)
+    {
+        cout << current->data << "  ";
+    }
+
+    TreeNode<T> *search(TreeNode<T> *current, T data)
+    {
+        if (current == NULL)
+            return NULL;
+        if (data == current->data)
+        {
+            return current;
+        }
+        else if (data < current->data)
+        {
+            search(current->left, data);
+        }
+        else
+        {
+            search(current->right, data);
+        }
     }
 };
 
 int main(int argc, char const *argv[])
 {
-
+    Tree<int> tree(8);                 // root값 8로 초기화
+    cout << tree.getRoot() << endl;    // root node의 주소값 불러오기
+    cout << tree.getRootVal() << endl; // root의 값을 불러오기
     return 0;
 }
