418cbd4be69b016299428df5a68ae257881ce15b
Neo_AlBan <albaneo0724@gmail.com>
2018-12-27 07:25:15 +0900

    tree

--- C++/beak_1068.cpp
+++ C++/beak_1068.cpp
@@ -1,50 +1,64 @@
 #include <fstream>
 #include <iostream>
 
-/**
- *  * 2차원 배열로 구현해볼까?
+/** 트리 백준_1068 (https://www.acmicpc.net/problem/1068)
+ *  * 트리를 배열로 구현해낼 수 있다.
  */
 
 using namespace std;
 
-int cnt, lim;
+int tree[51];
 
-int solution(int *arr, int idx)
+int solution(int *arr, int idx, int n)
 {
-    if (idx >= lim)
+    for (int i = idx; i <= n; i++)
     {
-        return 0;
-    }
-    else
-    {
-        arr[idx] = -2;
+        if (i * 2 <= n || (i * 2) + 1 <= n)
+        {
+            arr[i] = -2;
+            arr[i + 1] = -2;
+        }
     }
 
-    solution(arr, (idx * 2) + 1);
-    solution(arr, (idx * 2) + 2);
+    int ans = 0;
+
+    //  트리의 말단 노드 탐색을 어떻게 할것인가?
+    //  말단 노드의 조건 --> 자식 노드가 없다.
+    //  자식 노드가 없는 것의 판별은??  나의 인덱스 * 2 가 없고 나의 인덱스 *2 +1 도 없다 (-2 or NULL)
+    // TODO
+
+
+    for (int i = 1; i <= n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return ans;
 }
 
 int main(int argc, char const *argv[])
 {
     ifstream fs("input.txt");
-    int del;
-    cin >> lim;
+    int del, lim;
+    fs >> lim;
 
-    int *arr = new int[lim];
+    int *arr = new int[lim + 1];
 
-    for (int i = 0; i < lim; i++)
+    for (int i = 1; i <= lim; i++)
     {
-        cin >> arr[i];
+        fs >> arr[i];
     }
-    cin >> del;
-    solution(arr, del);
+    fs >> del;
+    cout << solution(arr, del + 1, lim) << endl;
 
-    for (int i = lim; i > 0; i--)
-    {
-        if (arr[i] != -2)
-        {
-            cnt++;
-        }
-    }
+    // for (int i = lim; i > 0; i--)
+    // {
+    //     if (arr[i] != -2)
+    //     {
+    //         cnt++;
+    //     }
+    // }
+
     return 0;
 }
