77b005de3275168913a393744c674aec84cdc21d
Neo_AlBan <albaneo0724@gmail.com>
2018-12-31 01:00:23 +0900

    jj

--- C++/Linked_List_Prac.cpp
+++ C++/Linked_List_Prac.cpp
@@ -9,76 +9,100 @@
 
 using namespace std;
 
-template <typename T>
-class Linked;
-
-template <typename T>
-class Node
+struct Node
 {
-
-    friend class Linked<T>;
-
   public:
-    Node(T data = 0, Node *next = NULL)
-    {
-        this->data = data;
-        this->next = next;
-    }
-
-  private:
-    T data;
+    int data;
     Node *next;
-    Node *tail;
-    Node *head;
 };
+
+// template <typename T>
+// class Linked;
+
+// template <typename T>
+// class Node
+// {
+
+//     friend class Linked<T>;
+
+//   public:
+//     Node(T data = 0, Node *next = NULL)
+//     {
+//         this->data = data;
+//         this->next = next;
+//     }
+
+//   private:
+//     T data;
+//     Node *next;
+// };
 
 template <typename T>
 class Linked
 {
   private:
-    Node<T> *head;
-    Node<T> *tail;
+    Node *head;
+    Node *tail;
 
-  public:
-    // // 생성자, 객체 생성시 입력된 값을 기준으로 head 노드 생성
-    // Linked(T data = 0)
-    // {
-    //     head = NULL;
-    //     tail = head; // 초기 값이 하나일 때에는 head와 tail은 같다.
-    // }
-
-    ~Linked() { cout << "Deleted the list " << endl; } //소멸자
-
-    Node<T> *getHead()
+    // 노드 전체 출력
+    void printAllList(Node *current)
     {
-        return head;
+        cout << current->data << " ";
+        if (current->next != nullptr)
+        {
+            printAllList(current->next);
+        }
     }
 
-    void insertNode(T val)
+    void insertNode(Node *newNode, T val)
     {
-        Node<T> *newNode = new Node<T>(val); // 새로운 노드 객체 생성
-        newNode->data = val;     // 데이터 저장
-        // newNode->next = NULL;             // 노드의 다음 변수 초기화
+        // Node *newNode = new Node; // 새로운 노드 객체 생성
+        newNode->data = val;  // 데이터 저장
+        newNode->next = nullptr; // 노드의 다음 변수 초기화
 
         // ! 왜 segment fault가? head에 next가 계속 생성되어있다?
-        if (head == NULL)
+        if (head->next == tail) // ! 얘가 문제다?
         {
-            head = newNode;
+            tail = newNode;
+            head->next = newNode;
         }
         else
         {
             // 꼬리를 설정하기 전에 새로운 노드를 먼저 꼬리노드의 다음노드로 선언해야 함
             tail->next = newNode;
+            tail = newNode; // 꼬리 노드를 새로 넣은 노드로 지정
         }
-        tail = newNode; // 꼬리 노드를 새로 넣은 노드로 지정
+
+        cout << "Inserted new Node" << endl;
+    }
+
+  public:
+    // 생성자, 객체 생성시 입력된 값을 기준으로 head 노드 생성
+    Linked()
+    {
+        head->next = nullptr;
+        tail = head->next; // 초기 값이 하나일 때에는 head와 tail은 같다.
+    }
+
+    ~Linked() { cout << "Deleted the list " << endl; } //소멸자
+
+    Node *getHead()
+    {
+        return head;
+    }
+
+    void insert(T val)
+    {
+        insertNode(new Node, val);
     }
 
     // 노드 삭제    REVIEW  need modify
-    void deleteNode(Node<T> *current, T val)
+    void deleteNode(Node *current, T val)
     {
-        if (val == current->data)
+        if (val == current->next->data)
         {
-            return current->next;
+            delete current->next;
+            current->next = current->next->next;
         }
         else
         {
@@ -86,14 +110,9 @@ class Linked
         }
     }
 
-    // 노드 전체 출력
-    void printList(Node<T> *current)
+    void printList()
     {
-        cout << current->data << " ";
-        if (current->next != NULL)
-        {
-            printList(current->next);
-        }
+        printAllList(getHead());
     }
 };
 
@@ -101,11 +120,11 @@ int main(int argc, char const *argv[])
 {
     Linked<int> *link;
 
-    link->insertNode(8);
-    link->insertNode(7);
-    link->insertNode(6);
+    link->insert(8);
+    // link->insertNode(7);
+    // link->insertNode(6);
 
-    link->printList(link->getHead());
+    link->printList();
 
     return 0;
 }
