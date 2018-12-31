88a9d6c4b98a51c5cffc57ff21a2cd755f4409c3
username <user@email.mail>
2018-12-31 15:40:06 +0900

    aaa

--- C++/beak_1068.cpp
+++ C++/beak_1068.cpp
@@ -7,37 +7,21 @@
 
 using namespace std;
 
-int tree[51];
-
-int solution(int *arr, int idx, int n)
+int solution(int* tree, int idx, int n)
 {
-    for (int i = idx; i <= n; i++)
+    int tmp = idx, ans = 0;
+    while (tmp <= n)
     {
-        if (i * 2 <= n || (i * 2) + 1 <= n)
-        {
-            arr[i] = -2;
-            arr[i + 1] = -2;
-        }
+        tmp *= 2;
+        tree[tmp] = -2;
+        tree[tmp + 1] = -2;
     }
 
-    int ans = 0;
-
-    //  트리의 말단 노드 탐색을 어떻게 할것인가?
-    //  말단 노드의 조건 --> 자식 노드가 없다.
-    //  자식 노드가 없는 것의 판별은??  나의 인덱스 * 2 가 없고 나의 인덱스 *2 +1 도 없다 (-2 or NULL)
-    // TODO
-
-    for(int i = n; i>0; i--){
-        if(arr[i/2] > -1 && arr[i*2])
+    for (int i = n; i > 0; i--)
+    {
+        if (tree[i / 2] > -1 && (tree[i * 2] == -2 && tree[(i * 2) + 1] == -2))
             ans++;
     }
-
-
-    for (int i = 1; i <= n; i++)
-    {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
 
     return ans;
 }
@@ -48,22 +32,21 @@ int main(int argc, char const *argv[])
     int del, lim;
     fs >> lim;
 
-    int *arr = new int[lim + 1];
+    int *tree = new int[101];
 
     for (int i = 1; i <= lim; i++)
     {
-        fs >> arr[i];
+        fs >> tree[i];
     }
     fs >> del;
-    cout << solution(arr, del + 1, lim) << endl;
 
-    // for (int i = lim; i > 0; i--)
-    // {
-    //     if (arr[i] != -2)
-    //     {
-    //         cnt++;
-    //     }
-    // }
+    // 전처리
+    for (int i = lim + 1; i <= 101; i++)
+    {
+        tree[i] = -2;
+    }
+
+    cout << solution(tree, del + 1, lim) << endl;
 
     return 0;
 }
