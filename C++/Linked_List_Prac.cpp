d32120ac3b8693e7bab2fe5668a2c4edfe47790f
Neo_AlBan <albaneo0724@gmail.com>
2019-01-02 15:56:33 +0900

    Linked List update

--- C++/Linked_List_Prac.cpp
+++ C++/Linked_List_Prac.cpp
@@ -19,7 +19,7 @@ class Node
 
     Node()
     {
-        next = NULL;
+        next = nullptr;
         data = -1; // 기본 초기화 값
     }
 
@@ -32,14 +32,15 @@ template <typename T>
 class Link
 {
   public:
-    void print()
+    void printNode()
     {
-        printNode(getHead());
+        print(head->next);
+        cout<<endl;
     }
 
     bool isEmpty()
     {
-        if (getHead()->next == NULL)
+        if (head->next == nullptr)
         {
             return true;
         }
@@ -49,14 +50,9 @@ class Link
         }
     }
 
-    Node<T> *getHead()
-    {
-        return this->head;
-    }
-
     Node<T> *search(Node<T> *current)
     {
-        if (current->next == NULL)
+        if (current->next == nullptr)
         {
             return current;
         }
@@ -64,38 +60,65 @@ class Link
         search(current->next);
     }
 
+    void insert(T val)
+    {
+        insertNode(val);
+    }
+
     int size()
     {
         Count = 0;
-        Node<T> *temp = getHead()->next;
+        Node<T> *temp = head;
 
-        while (temp->next != NULL)
+        while (temp->next != nullptr)
         {
             temp = temp->next;
             Count++;
         }
+        return Count;
+    }
+
+    Link()
+    {
+        head->next = nullptr;
+        tail->next = nullptr;
+    }
+
+    // TODO check this to exactly active
+    ~Link()
+    {
+        Node<T> *temp = head;
+        while (temp->next != nullptr)
+        {
+            Node<T> *temp1 = temp->next;
+            delete temp;
+            temp = temp1->next;
+        }
+        delete tail;
     }
 
   private:
-    Node<T> *haed = new Node<T>();
+
+    Node<T> *head = new Node<T>();
     Node<T> *tail = new Node<T>();
     int Count;
 
-    void printNode(Node<T> *current)
+    void print(Node<T> *current)
     {
         cout << current->data << " ";
-        if (current->next != NULL)
+        if (current->next != nullptr)
         {
-            printNode(current->next);
+            print(current->next);
         }
     }
 
     void insertNode(T data)
     {
-        Node<T> *temp = getHead()->next; // temp : 제일 첫번째 값이 들어가있는 노드
+        // Node<T> *temp = getHead()->next; // temp : 제일 첫번째 값이 들어가있는 노드
+        Node<T> *temp = head;
 
         //  다음을 가리키는 포인터가 비어있는 노드를 반복문을 돌며 찾는다
-        while (temp->next != NULL)
+        while (temp->next != nullptr)
         {
             temp = temp->next;
         }
@@ -109,5 +132,17 @@ class Link
 
 int main(int argc, char const *argv[])
 {
+    Link<int> *link = new Link<int>();
+
+    link->insert(8);
+    link->insert(9);
+    link->insert(10);
+
+    cout << "Value in List : ";
+    link->printNode();
+
+    cout << "List Size : " << link->size() << endl;
+    link->isEmpty();
+
     return 0;
 }
