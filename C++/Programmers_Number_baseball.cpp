40ca38b87b2a90a10065daa9e74ea53419d23af4
Neo_AlBan <albaneo0724@gmail.com>
2019-01-09 21:44:04 +0900

    baseball update

--- C++/Programmers_Number_baseball.cpp
+++ C++/Programmers_Number_baseball.cpp
@@ -1,39 +1,58 @@
+#include <algorithm>
 #include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>
 
 /**
- *  * 모든 상황을 보고 판단해야한다
- *  * [n][1] && [0][2] 가 0이면 아웃
- *  ? 스트라이크와 볼에 가중치 각각 2,1을 부여
- *  ? 가중치 순서대로 나열하여 바뀐것을 비교하며 답이 될 수 있는 것을 찾아간다
- *  ? 재귀적으로 풀면 어떨까?? like permutation
+ *  * 숫자의 범위 : 000 ~ 999, 이들을 모두 판별
  */
 
 using namespace std;
+
+vector<string> arr;
+
+void func()
+{
+    string temp = "";
+
+    for (int i = 0; i <= 999; i++)
+    {
+        if ((i / 100) < 1)
+        {
+            if ((i / 10) >= 1)
+            {
+                temp += ("0" + to_string(i));
+            }
+            else
+            {
+                temp += ("00" + to_string(i));
+            }
+        }
+        else
+        {
+            temp += to_string(i);
+        }
+    }
+    arr.push_back(temp);
+}
 
 int solution(vector<vector<int>> baseball)
 {
     int answer = 0;
     int bSize = baseball.size();
-    int *temp = new int[bSize];
+    func();
+    int arrSize = arr.size();
+    vector<int> temp;
+    // compare baseball[i][0] to femp
+    // how to use the baseball vector
 
-    for(int i = 0; i<bSize; i++)
+    for (int i = 0; i < bSize; i++)
     {
-        temp[i] = (baseball[i][1] * 2) +(baseball[i][2] *1);
+        temp.push_back((baseball[i][1] * 3) + baseball[i][2]);
     }
 
-    auto itr = max_element(temp, temp+bSize);
 
-    if(*itr >= 5)
-    {
 
-    }
-    else
-    {
-
-    }
 
     return answer;
 }
