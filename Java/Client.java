4caf6cc6fd6a2be420c31f60f653bdb16b8775e1
Neo_AlBan <albaneo0724@gmail.com>
2019-01-13 00:46:52 +0900

    java socket communication update

---
+++ Java/Client.java
@@ -0,0 +1,46 @@
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.InetSocketAddress;
+
+/**
+ *  * 전송시 OutputStream /수신시 InputStream 을 이용하여 송수신
+ *  * 수신 read(byte, offset, length); / 송신 write(byte, offset, length); 사용
+ *  * 소켓 통신을 더욱 OOP스럽게 하려면, 데이터 헤더 클래스를 이용
+ */
+
+public class Client {
+    public static void main(String[] args) {
+        int PORT = 9999; // 서버 port
+        String addr = "127.0.0.1"; // 서버 address
+
+        // 자동 close, 클라이언트 소켓 선언
+        try (Socket client_Socket = new Socket()) {
+            // 클라이언트 소켓 초기화
+            InetSocketAddress ipep = new InetSocketAddress(addr, PORT);
+            // 접속 ( Connect )
+            client_Socket.connect(ipep);
+
+            // sender, receiver stream 받아오기
+            try (OutputStream sender = client_Socket.getOutputStream();
+                    InputStream receiver = client_Socket.getInputStream();) {
+                // 서버로부터 데이터 받기
+                byte[] data = new byte[100];
+                receiver.read(data, 0, 100);    // byte, offset, length
+
+                // 수신 메세지 출력
+                String msg = new String(data);
+                String out = String.format("receive - %s", msg);    // ? what is 'String.format();'
+                System.out.println(out);
+
+                // 서버로 데이터 보내기
+                // 메세지 선언 --> 메시지를 바이트 배열에 할당 --> 전송(write)
+                msg = "OK";
+                data = msg.getBytes();
+                sender.write(data, 0, data.length);
+            }
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Java/Server.java
@@ -0,0 +1,48 @@
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *  * 전송시 OutputStream /수신시 InputStream 을 이용하여 송수신
+ *  * 소켓 통신을 더욱 OOP스럽게 하려면, 데이터 헤더 클래스를 이용
+ */
+
+public class Server {
+    public static void main(String[] args) {
+
+        int PORT = 9999;
+
+        // 자동 close, 서버 소켓 선언
+        try (ServerSocket server_Socket = new ServerSocket()) {
+            // 서버 소켓 초기화
+            InetSocketAddress ipep = new InetSocketAddress(PORT);
+            server_Socket.bind(ipep); // InetSocketAddress 객체와 바인드
+
+            System.out.println("Initialize coplete");
+
+            // Server listenning, wiat for accept
+            Socket client_Socket = server_Socket.accept();
+            System.out.println("Connection");   // print connection message, when client accept
+
+            // send, receiver stream 받아오기
+            try (OutputStream sender = client_Socket.getOutputStream();
+                    InputStream receiver = client_Socket.getInputStream();) {
+                // 클라이언트로 메시지 보내기
+                String msg = "Socket Transfer Test";
+                byte[] data = msg.getBytes();   // 메시지 길이만큼 byte배열 생성
+                sender.write(data, 0, data.length); // byte, offset, length
+
+                data = new byte[2];
+                receiver.read(data, 0, data.length);// byte, offset, length
+
+                msg = new String(data);
+                String out = String.format("recieve - %s", msg);    // ? what is 'String.format();'
+                System.out.println(out);
+            }
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+}
