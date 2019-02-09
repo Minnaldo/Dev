c76a67714a2b4cfa502b1729b109b9a1e81ce2ac
Neo_AlBan <albaneo0724@gmail.com>
2019-02-09 12:57:31 +0900

    a

--- C++/programmers_kakao_colorlingbook.cpp
+++ C++/programmers_kakao_colorlingbook.cpp
@@ -3,6 +3,10 @@
 #include <queue>
 #include <vector>
 
+/** 프로그래머스_컬러링북 (https://programmers.co.kr/learn/courses/30/lessons/1829)
+ *  ? BFS 응용
+ */
+
 using namespace std;
 
 int dir[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
@@ -12,9 +16,8 @@ vector<int> solution(int m, int n, vector<vector<int>> picture)
     int number_of_area = 0;
     int max_size_of_one_area = 0;
 
-    serachElement(m, n, picture); // 원소 종류를 element 전역 벡터에 저장
-
     queue<pair<int, int>> q;
+    queue<pair<int, int>> nxt_q;
     q.push(make_pair(0, 0));
 
     int last_y = 0, last_x = 0, tmpQsize = 0;
@@ -35,40 +38,28 @@ vector<int> solution(int m, int n, vector<vector<int>> picture)
 
                 if (nxt_y < m && nxt_y >= 0 && nxt_x < n && nxt_x >= 0)
                 {
-                    if (picture[cur_y][cur_x] == picture[nxt_y][nxt_x] && picture[cur_y][cur_x] != 0) // decision the section that has same color
+                    if (picture[cur_y][cur_x] == picture[nxt_y][nxt_x] && picture[nxt_y][nxt_x] != 0) // decision the section that has same color
                     {
                         q.push(make_pair(nxt_y, nxt_x));
                         picture[cur_y][cur_x] = 0; // 더이상 탐색하지 않게 초기화
                     }
-                }
-            }
 
-            if (tmpQsize < q.size()) // section size update
-            {
-                tmpQsize = q.size();
-            }
-            last_y = q.back().first;
-            last_x = q.back().second;
-
-            if (q.size() <= 1)
-            {
-                for (int i = 0; i < 4; i++)
-                {
-                    int nxt_last_y = last_y + dir[i][0];
-                    int nxt_last_x = last_x + dir[i][1];
-
-                    if (nxt_last_y >= 0 && nxt_last_y < m && nxt_last_x >= 0 && nxt_last_x < n)
+                    if (picture[cur_y][cur_x] != picture[nxt_y][nxt_x] && picture[nxt_y][nxt_x] != 0)
                     {
-
+                        nxt_q.push(make_pair(nxt_y, nxt_x));
                     }
                 }
             }
 
+            tmpQsize++;
             q.pop();
         }
         number_of_area++;
+        max_size_of_one_area = tmpQsize;
 
-        q.push(make_pair(last_y, last_x));
+        tmpQsize = 0;
+
+        q.push(make_pair(nxt_q.front().first, nxt_q.front().second));
     }
 
     vector<int> answer(2);
@@ -80,5 +71,15 @@ vector<int> solution(int m, int n, vector<vector<int>> picture)
 
 int main(int argc, char const *argv[])
 {
+    int m = 6, n = 4;
+    vector<vector<int>> picture = {{1, 1, 1, 0}, {1, 2, 2, 0}, {1, 0, 0, 1}, {0, 0, 0, 1}, {0, 0, 0, 3}, {0, 0, 0, 3}};
+
+    vector<int> answer = solution(m, n, picture);
+
+    for (int i = 0; i < 2; i++)
+    {
+        cout << answer[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
