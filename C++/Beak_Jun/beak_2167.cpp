2a297650605f1442f95c910966085305c7967801
username <user@email.mail>
2019-02-16 19:46:19 +0900

    update

--- C++/Beak_Jun/beak_2167.cpp
+++ C++/Beak_Jun/beak_2167.cpp
@@ -1,42 +1,54 @@
+#include <fstream>
 #include <iostream>
+
+/** 백준_2167 2차원 배열의 합 ( https://www.acmicpc.net/problem/2167 )
+ *  * Dynamic Porgramming ???
+ */
 
 using namespace std;
 
-int dp[301][301];
+int dp[300][300];
 
-int solution(int** arr, int i, int j, int x, int y)
+int solution(int **arr, int i, int j, int x, int y)
 {
-    dp[i][j] = arr[i][j];
+    int answer = 0;
 
+    cout << i << j << x << y << endl;
 
+    for (int a = i; a <= x; a++)
+    {
+        for (int b = j; b <= y; b++)
+        {
+            answer += arr[a][b];
+        }
+    }
+
+    return answer;
 }
 
 int main(int argc, char const *argv[])
 {
+    ios::sync_with_stdio(false);
 
-    int N, M;
+    int K, N, M, a, b, c, d;
+    cin >> N >> M;
 
-    cin>>N>>M;
-
-    int** arr = new int* [N+1];
-    for(int i = 1; i<=N; i++)
+    int **arr = new int *[N+1];
+    for (int i = 1; i <= N; i++)
     {
-        arr[i] = new int [M+1];
-        for(int j = 1; j<=M; j++)
+        arr[i] = new int[M+1];
+        for (int j = 1; j <= M; j++)
         {
-            int tmp;
-            cin>>tmp;
-            arr[i][j] = tmp;
+            cin >> arr[i][j];
         }
     }
 
-    int K;
-    cin>>K;
-    for(int i = 0; i<K; i++)
+    cin >> K;
+    for (int i = 0; i < K; i++)
     {
-        int a, b, c, d;
-        cin>>a>>b>>c>>d;
-        cout<<solution(arr, a, b, c, d)<<endl;
+        cin >> a >> b >> c >> d;
+
+        cout << solution(arr, a, b, c, d) << endl;
     }
 
     return 0;
