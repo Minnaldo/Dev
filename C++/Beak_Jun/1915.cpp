0bf004f42accc2466e715918f67f2f9ac84aca67
username <user@email.mail>
2019-02-27 17:53:27 +0900

    Update 1915.cpp

--- C++/Beak_Jun/1915.cpp
+++ C++/Beak_Jun/1915.cpp
@@ -1,35 +1,69 @@
 #include <fstream>
 #include <iostream>
+#include <queue>
 #include <string>
+
+/** 가장 큰 정사각형 백준_1915 ( https://www.acmicpc.net/problem/1915 )
+ *  * way 1) 가로 세로 크기만 구해서 곱한다.
+ *  * way 2) 확실한 크기를 구한다
+ *  * DP + DFS?? / DP + BFS?
+ */
 
 using namespace std;
 
 int N, M;
 int dir[4][2] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-bool chk[1001][1001];
+int dp[1001][1001];
 
 int max(int &a, int &b) { return a > b ? a : b; }
 
 // TODO how to search for square in 2-dimetion array
-int solution(int **arr, int y, int x, int mul)
+int solution(int **arr, int y, int x)
 {
     int tmp = 0;
-    for (int i = y; i <= mul; i++)
+    queue<pair<int, int>> q;
+    q.push(make_pair(y, x));
+    dp[y][x] = 1;
+
+    while (!q.empty())
     {
-        for (int j = x; j <= mul; j++)
+        y = q.front().first, x = q.front().second;
+        for (int i = 0; i < 4; i++)
         {
-            if (arr[i][j] == 1)
-                if (i > 0 && j > 0 && i <= N && j <= M)
-                    tmp++;
+            int nx = x + dir[i][1], ny = y + dir[i][0];
+
+            if (ny > 0 && nx > 0 && ny <= M && nx <= N)
+            {
+                if (arr[ny][nx] == 1)
+                {
+                    q.push(make_pair(ny, nx));
+                    dp[ny][nx] = dp[y][x] + 1;
+                }
+            }
+        }
+
+        q.pop();
+    }
+
+    return dp[y][x];
+}
+
+int dfs(int **arr, int y, int x){
+    if(dp[y][x]) return dp[y][x];
+
+    dp[y][x] = 1;
+
+    for(int i = 0; i<4; i++)
+    {
+        int nx = x + dir[i][1], ny = y + dir[i][0];
+        if (ny > 0 && nx > 0 && ny <= M && nx <= N)
+        if(arr[ny][nx] == 1)
+        {
+            dp[y][x] = max(dp[y][x], dfs(arr, ny, nx)+1);
         }
     }
 
-    if (tmp % mul == 0)
-    {
-        return tmp;
-    }
-
-    return 0;
+    return dp[y][x];
 }
 
 int main(int argc, char const *argv[])
@@ -59,8 +93,9 @@ int main(int argc, char const *argv[])
     {
         for (int j = 1; j <= M; j++)
         {
-            for (int k = 1; k <= N || k <= M; k++)
-                answer = max(answer, solution(arr, i, j, k));
+            if (arr[i][j] == 1)
+                // answer = max(answer, solution(arr, i, j));
+                answer = max(answer, dfs(arr, i, j));
         }
     }
 
