08c718eeabe16afe6d25fcfc38707a051fcc4d02
Neo_AlBan <albaneo0724@gmail.com>
2019-03-01 03:25:55 +0900

    Update 11066.cpp

--- C++/Beak_Jun/11066.cpp
+++ C++/Beak_Jun/11066.cpp
@@ -4,38 +4,27 @@
 /**
  *  * 모든 과정에서 생성되는 임시파일 크기의 합의 최소를 구하는 문제
  *  * 바로 옆의 파일만 합칠 수 있다.
+ *  * 파일 + 파일, 파일 + 임시파일( or 임시파일 + 파일 ), 임시파일 + 임시파일
+ *  TODO i re-think for logic
  */
 
 using namespace std;
 
 long long dp[10001][10001];
 
-int min(int &a, int &b) { return a < b ? a : b; }
+// int min(int &a, int &b) { return a < b ? a : b; }
 
 long long solution(int *arr, int n)
 {
     int answer = 0;
 
-    for (int j = 1; j < n; j++)
-    {
-        // 파일끼리 합쳤을 때, when merge between the file size
-        dp[j][1] = arr[j] + arr[j + 1];
-    }
-
-    for (int i = 2; i <= n; i++)
-    {
-        for (int j = 1; j < n; j++)
-        {
-            dp[j][i] = min(dp[j][i] + arr[j - 1], dp[j][i] + arr[j + 1]);
-            dp[j][i] = min(dp[j][i] + arr[j + 2], dp[j][i] + arr[j - 1]);
-        }
-    }
-
-    answer = dp[2][n];
     for (int i = 1; i <= n; i++)
     {
-        if (answer > dp[i][n] && dp[i][n] != 0)
-            answer = dp[i][n];
+        for (int j = 1; j <= n; j++)
+        {
+            int tmp = (j + 3) % n;
+            dp[j][i] = min(dp[j][i - 1] + dp[j + 1][i - 1], dp[j][i] + dp[j + 1][i]);
+        }
     }
 
     return answer;
@@ -48,16 +37,20 @@ int main(int argc, char const *argv[])
     int T, N;
     fs >> T;
 
+    // 2개의 테스트 케이스를 위해 i < 1 ==> i < 2
     for (int i = 0; i < 1; i++)
     {
         fs >> N;
 
         int *arr = new int[N + 1];
+        arr[0] = 0;
         for (int j = 1; j <= N; j++)
         {
             fs >> arr[j];
+            cout << arr[j] << " ";
             dp[j][0] = arr[j];
         }
+        cout << endl;
 
         cout << solution(arr, N) << endl;
     }
