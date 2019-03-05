31ade0bb42e4a828d2c8fe707da4dba8a1935887
Neo_AlBan <albaneo0724@gmail.com>
2019-03-06 01:32:27 +0900

    Minimum Heap update

--- C++/Data Structure/Heap.cpp
+++ C++/Data Structure/Heap.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <queue>
 
 /**
- *  TODO implement the heap structure
+ *  * 최대 힙 Maximum_Heap
+ *  TODO code test
  */
 
 using namespace std;
@@ -19,13 +21,15 @@ class Node
     {
         right = nullptr;
         left = nullptr;
-        data = INT32_MIN;
+        parent = nullptr;
+        data = INT32_MAX;
     }
 
     Node(T val)
     {
         right = nullptr;
         left = nullptr;
+        parent = nullptr;
         data = val;
     }
 
@@ -33,6 +37,7 @@ class Node
     T data;
     Node<T> *right;
     Node<T> *left;
+    Node<T> *parent;
 };
 
 template <typename T>
@@ -44,47 +49,101 @@ class Heap
         root = new Node<T>();
         Count = 0;
     }
-    void insNode(T val)
+    void insert(T val)
     {
+        Node<T> *tmp = root;
+        Node<T> *parent = tmp->parent;
         Node<T> *newNode = new Node<T>(val);
 
-        Node<T> *tmp = search(val);
-
-        if (newNode == nullptr)
+        while (tmp != nullptr)
         {
-            newNode->data = val;
+            parent = tmp;
+            tmp = (tmp->left == nullptr) ? tmp->left : tmp->right;
         }
+
+        tmp = newNode;
+        tmp->parent = parent;
+
+        update(tmp); // 최소힙을 만족하기 위해 데이터를 업데이트
+
         Count++
     }
-    void delNode() { Count-- }
-    void search(T val)
+
+    void delNode() // 최대 힙에서 삭제연산은 최대 값을 삭제하는 것이다.
     {
-        search(root, val);
+        cout << root->data << " has been deleted" << endl;
+        Node<T> *last = find_last();
+        root->data = last->data;
+
+        Node<t> *tmp = root;
+
+        while (tmp != nullptr)
+        {
+            if (tmp->data > tmp->left->data)
+            {
+                swap(tmp->data, tmp->left->data);
+                tmp = tmp->left;
+            }
+            else if (tmp->data > tmp->right->data)
+            {
+                swap(tmp->data, tmp->right->data);
+                tmp = tmp->right;
+            }
+        }
+
+        Count--;
+    }
+
+    Node<T> *find_last()
+    {
+        Node<T> *last;
+        queue<Node<T> *> q;
+        q.push(root);
+
+        while (!q.empty())
+        {
+            Node<T> *current = q.front();
+
+            if (current->left != nullptr)
+                q.push(current->left);
+            if (current->right != nullptr)
+                q.push(current->right);
+
+            last = q.back();
+            q.pop();
+        }
+        (last->parent->left == last) ? (last->parent->left = nullptr) : (last->parent->right = nullptr);
+        return last;
+    }
+
+    void update(Node<T> *current)
+    {
+        while (current->parent != nullptr) // current 노드가 root 노드가 되면 끝냄
+        {
+            Node<T> *parent = current->parent;
+            if (current->data < parent->data)
+            {
+                swap(current->data, parent->data);
+            }
+
+            current = parent;
+        }
+    }
+
+    int size()
+    {
+        return Count;
     }
 
   private:
     Node<T> *root;
     int Count;
 
-    Node<T> *search(Node<T> *tmp, T val)
+    void swap(int &a, int &b)
     {
-        if (tmp->data == val)
-        {
-            return tmp;
-        }
-
-        if (val < tmp->data)
-        {
-            if (tmp->left == nullptr)
-                return tmp->left;
-            search(tmp->left, val);
-        }
-        else
-        {
-            if (tmp->right == nullptr)
-                return tmp->right;
-            search(tmp->right, val);
-        }
+        int tmp = a;
+        a = b;
+        b = tmp;
     }
 };
 
