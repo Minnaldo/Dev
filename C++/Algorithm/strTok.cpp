5bd9bfc055c599b5e7cb6523ca0d4b1074b7fda6
AlBaneo93 <albaneo0724@gmail.com>
2019-03-21 09:19:28 +0900

    tokenizer update

--- C++/Algorithm/strTok.cpp
+++ C++/Algorithm/strTok.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
 
 using namespace std;
 
+/**
+ *  * stringstream 을 이용한  Tokenizer
+ *  * 템플릿 함수를 이용하여 가용성 높임
+ *  * but, delemeter 지정 불가
+ *  * string에서 int형만 빼고 싶을 때, int형 자료 앞에 다른 형태가 있으면 끊어짐 (when use while loop)
+ */
+template <typename T>
+vector<T> sstrtok(string str)
+{
+    vector<T> result;
+    stringstream ss;
+    ss.str(str);
+    T type;
+    while (ss >> type)  // TODO FIXME
+    {
+        result.push_back(type);
+    }
+
+    return result;
+}
+
+/**
+ *  * string iterator를 이용한 tokenizer
+ */
 vector<string> strtok(string str, char delemeter = ' ')
 {
     vector<string> result;
@@ -32,12 +57,23 @@ vector<string> strtok(string str, char delemeter = ' ')
 int main(int argc, char const *argv[])
 {
     vector<string> ans;
-    ans = strtok("abc bcd 178");
+    vector<float> ans2;
+    // ans = strtok("abc bcd 178");
+    string tmp;
+    ans = sstrtok<string>("abc 123 ABC");
+    ans2 = sstrtok<float>("abc 123 ABC");
 
     int size = ans.size();
     for (int i = 0; i < size; i++)
     {
         cout << ans[i] << endl;
+    }
+
+    size = ans2.size();
+    cout << "ans2 size : " << size << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cout << ans2[i] << endl;
     }
     return 0;
 }
