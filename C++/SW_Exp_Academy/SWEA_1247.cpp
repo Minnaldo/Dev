655de17ab1e76e63eb1829ea36f5bb57cb2722c8
AlBaneo93 <albaneo0724@gmail.com>
2019-04-30 17:23:44 +0900

    aa

---
+++ C++/SW_Exp_Academy/SWEA_1247.cpp
@@ -0,0 +1,109 @@
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <iostream>
+#include <vector>
+
+/**
+ *
+ */
+
+using namespace std;
+
+int t, n, curMin, dst[10][10];
+bool visit[10];
+vector<pair<int, int>> arr;
+pair<int, int> office, home;
+
+void init()
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j)
+            {
+                dst[i][j] = 0;
+            }
+            else
+            {
+                dst[i][j] = abs(arr[i].first - arr[j].first) + abs(arr[i].second - arr[j].second);
+            }
+        }
+    }
+}
+
+int solution(int curIdx, int sum)
+{
+    int nxtIdx = -1;
+    int tmpdst = 9999;
+    for (int i = 0; i < n; i++)
+    {
+        if (i != curIdx && !visit[i])
+        {
+            if (tmpdst > dst[curIdx][i])
+            {
+                nxtIdx = i;
+            }
+        }
+    }
+
+    if (nxtIdx != -1)
+    {
+        visit[nxtIdx] = true;
+        solution(nxtIdx, sum + dst[curIdx][nxtIdx]);
+    }
+    else
+    {
+        return sum += (abs(arr[curIdx].first - home.first) + abs(arr[curIdx].second - home.second));
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    freopen("input.txt", "r", stdin);
+
+    scanf("%d", &t);
+
+    for (int tc = 1; tc <= t; tc++)
+    {
+        scanf("%d", &n);
+
+        scanf("%d %d", &office.first, &office.second);
+        scanf("%d %d", &home.first, &home.second);
+
+        arr.assign(n, pair<int, int>());
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d %d", &arr[i].first, &arr[i].second);
+        }
+
+        init();
+        int idx = 0;
+        int tmpdst = abs(office.first - arr[0].first) + abs(office.second - arr[0].second);
+        for (int i = 1; i < n; i++)
+        {
+            if (abs(office.first - arr[i].first) + abs(office.second - arr[i].second) < tmpdst)
+            {
+                idx = i;
+                tmpdst = abs(office.first - arr[i].first) + abs(office.second - arr[i].second);
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                cout << dst[i][j] << " ";
+            }
+            cout << endl;
+        }
+
+        // visit[idx] = true;
+        // int ans = solution(idx, tmpdst);
+
+        // printf("#%d %d\n", tc, ans);
+    }
+
+    return 0;
+}
--- C++/SW_Exp_Academy/SWEA_2819.cpp
+++ C++/SW_Exp_Academy/SWEA_2819.cpp
@@ -1,43 +1,37 @@
 #include <algorithm>
 #include <iostream>
-#include <string>
 #include <vector>
 
-/**
- *
+/** 격자판의 숫자 이어 붙이기 SWEA_2819 ( https://www.swexpertacademy.com/main/code/problem/problemDetail.do )
  */
 
 using namespace std;
 
 int t, map[4][4]; // 맵
 int dir[4][2] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
-vector<string> arr; // 수를 저장할 배열
+vector<int> arr; // 수를 저장할 배열
 
-void dfs(string tmp, int y, int x)
+void dfs(int tmp, int y, int x, int cnt)
 {
-    if (tmp.size() == 7)
+    if (cnt == 6)
     {
         arr.push_back(tmp);
+        return;
     }
-    else if (tmp.size() < 7)
+    for (int i = 0; i < 4; i++)
     {
-        for (int i = 0; i < 4; i++)
-        {
-            int ny = y + dir[i][0];
-            int nx = x + dir[i][1];
+        int ny = y + dir[i][0];
+        int nx = x + dir[i][1];
 
-            if (ny >= 0 && nx >= 0 && ny < 4 && nx < 4)
-            {
-                tmp += to_string(map[ny][nx]);
-                dfs(tmp, ny, nx);
-            }
+        if (ny >= 0 && nx >= 0 && ny < 4 && nx < 4) // 격자 범위 내일 때
+        {
+            dfs(tmp * 10 + map[ny][nx], ny, nx, cnt + 1);
         }
     }
 }
 
 int main(int argc, char const *argv[])
 {
-    freopen("input.txt", "r", stdin);
     scanf("%d", &t);
 
     for (int tc = 1; tc <= t; tc++)
@@ -48,22 +42,14 @@ int main(int argc, char const *argv[])
             for (int j = 0; j < 4; j++)
                 scanf("%d", &map[i][j]);
 
+        // 루틴 시작
         for (int i = 0; i < 4; i++)
             for (int j = 0; j < 4; j++)
-            {
-                string tmp = "";
-                tmp += to_string(map[i][j]);
-                dfs(tmp, i, j);
-            }
+                dfs(map[i][j], i, j, 0);
 
         sort(arr.begin(), arr.end());
         arr.erase(unique(arr.begin(), arr.end()), arr.end());
         ans = arr.size();
-
-        for (int i = 0; i < arr.size(); i++)
-        {
-            cout << arr[i] << endl;
-        }
 
         printf("#%d %d\n", tc, ans);
         arr.clear();
