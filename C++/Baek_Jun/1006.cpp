6b0884108c865ebf571d6c82758b54a648e40875
AlBaneo93 <albaneo0724@gmail.com>
2019-05-15 14:08:11 +0900

    a

--- C++/Baek_Jun/1006.cpp
+++ C++/Baek_Jun/1006.cpp
@@ -1,62 +1,50 @@
-#include <cstring>
-#include <deque>
 #include <iostream>
-
-// 인접 영역 커버 가능? ==> 가장 많은 적을 처리할 수 있는 영역 선택
-// 인접 영역 커버 불가? ==> 해당 구역인덱스와 같은 visit배열 인덱스를 true로 체크 후 넘어감
+#include <vector>
 
 using namespace std;
 
 int t, ans, n, w;
-bool visit[2][10000];
+vector<vector<int>> section;
+vector<bool> visit;
 
-int solution(deque<deque<int>> dq)
+int min(int &a, int &b) { return a < b ? a : b; }
+
+// 최대한 많은 소대가 2개 구역을 맡아야 최소값이 나온다
+int solution()
 {
+    int cnt = 0;
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < 2; j++)
-        {
-            int nxt, pre;
-            if (i == 0)
-            {
-                pre = n - 1;
-                nxt = i + 1;
-            }
-            else if (i == n - 1)
-            {
-                pre = i - 1;
-                nxt = 0;
-            }
-            else
-            {
-                pre = i - 1;
-                nxt = i + 1;
-            }
-        }
     }
+
+    return cnt;
 }
 
 int main(int argc, char const *argv[])
 {
     freopen("input.txt", "r", stdin);
+    scanf("%d", &t);
 
     while (t--)
     {
         ans = 0;
-        memset(visit, false, sizeof(visit));
         scanf("%d %d", &n, &w);
-
-        deque<deque<int>> dq(2, deque<int>(n));
 
         for (int i = 0; i < 2; i++)
         {
             for (int j = 0; j < n; j++)
             {
-                scanf("%d", &dq[i][j]);
+                int tmp;
+                scanf("%d", &tmp);
+                section[i].push_back(tmp);
             }
         }
 
+        ans = n * 2; // 이론상 최댓값
+
+        ans = min(ans, solution());
         printf("%d\n", ans);
+        section.clear();
     }
 
     return 0;
