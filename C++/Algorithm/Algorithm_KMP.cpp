3704d7acfbdac9880f17d418b47017575905dcfe
AlBaneo93 <albaneo0724@gmail.com>
2019-05-30 15:20:59 +0900

    kmp update

--- C++/Algorithm/Algorithm_KMP.cpp
+++ C++/Algorithm/Algorithm_KMP.cpp
@@ -11,9 +11,9 @@ using namespace std;
  */
 
 //  KMP pre-processing
-//  접두사와 접미사의 최대 일치길이를 구한다.
+//  패턴 문자열에서 접두사와 접미사의 최대 일치길이를 구한다.
 //  i인덱스와 j인덱스의 문자열이 같으면 둘 다 증가, 아니면 i만 증가
-//  fail[index]의 value 만큼 일치를 했다.
+//  fail[index]의 value 만큼 일치를 했다. 일치하는 접두사, 접미사의 길이가 fail[idx]의 값 만큼이다.
 vector<int> preProcessing(string pattern)
 {
     int j = 0; // j ==> 접두사의 길이 인덱스
@@ -23,17 +23,6 @@ vector<int> preProcessing(string pattern)
 
     //  i는 왜 1에서부터 시작을 해야하나??
     //  i와 j가 같으면, 모든 문자열을 검사하게 되므로, 알고리즘이 의미가 없다
-    // for (int i = 1; i < pSize; i++)
-    // {
-    //     while (j > 0 && (pattern[i] != pattern[j])) // j인덱스의 문자와 i인덱스 문자가 다를 때
-    //     {
-    //         j = fail[j - 1]; // 비교한 문자열이 다를경우 실패함수에 이전 인덱스의 값을 j 값으로 한다
-    //     }
-
-    //     // 일치시, j인덱스의 값에 1을 더해 실패함수에 저장
-    //     if (pattern[j] == pattern[i])
-    //         fail[i] = ++j; // j++, fail[i] = j;
-    // }
 
     int i = 1;
     while (i < pSize)
@@ -44,8 +33,9 @@ vector<int> preProcessing(string pattern)
 
         if (pattern[j] == pattern[i])
         {
-            fail[i] = ++j; // fail[i] = j+1; j++;, 현재 j인덱스에 1을 더한 값을 fail함수의 값으로 쓴다(j의 인덱스가 최대 일치길이, j가 0부터 시작이기 때문에 1을 더해준다)
+            // 현재 j인덱스에 1을 더한 값을 fail함수의 값으로 쓴다(j의 인덱스가 최대 일치길이, j가 0부터 시작이기 때문에 1을 더해준다)
             // j는 이전 문자의 실패함수 값, 실패함수 값이 계속 증가한다면 일치하는 접미사가 있는 접두사의 길이가 증가한다는 뜻
+            fail[i] = ++j; // fail[i] = j+1; j++;
         }
         i++;
     }
@@ -58,39 +48,31 @@ vector<int> KMP(string body, string pattern)
     int j = 0; // 패턴의 인덱스
     int bodySize = body.size();
     int patternSize = pattern.size();
-    vector<int> fail = preProcessing(pattern);
-    vector<int> answer;
-    // for (int i = 0; i < bodySize; i++) // i ==> parent의 인덱스
-    // {
-    //     while (j > 0 && pattern[j] != body[i]) // 패턴과 문자열이 다를 때
-    //         j = fail[j - 1];
+    vector<int> fail = preProcessing(pattern); // 실패함수를 저장할 배열
+    vector<int> answer;                        // 일치하는 위치의 시작 인덱스를 저장할 배열
 
-    //     if (body[i] == pattern[j])
-    //         if (j == patternSize - 1)
-    //         {
-    //             printf("%d번재에서 찾았습니다.\n", i - patternSize + 2);
-    //             j = fail[j];
-    //         }
-    //         else
-    //             j++;
-    // }
-    int i = 0; // body의 인덱스
+    int i = 0; // 본문의 인덱스
     while (i < bodySize)
     {
         // 패턴과 본문이 다른 인덱스를 찾는다
         while (j > 0 && body[i] != pattern[j]) // 패턴과 문자열이 다를 때
             j = fail[j - 1];                   // j = 패턴의 인덱스 pattern[j]부터 다시 찾기 시작한다.
 
-        if (body[i] == pattern[j])    // 본문과 패턴의 문자가 일치할 때
+        if (body[i] == pattern[j]) // 본문과 패턴의 문자가 일치할 때
+        {
             if (j == patternSize - 1) // j는 0부터 시작, if 문을 만족할 경우 본문에서 패턴과 일치하는 부분을 찾은게 됨
             {
-                printf("%d번째에서 찾았습니다.\n", i - patternSize + 2); // 인덱스가 0부터 시작이므로 + , 첫 시작부분을 포함해야하므로 또 +1 ==> +2를 해주어야 한다
-                j = fail[j];                                             // 다음 일치 부분을 찾기 위해 다음 일치지점부터 찾기 시작한다.
+                printf("%d번째에서 찾았습니다.\n", i - patternSize + 2); // 인덱스가 0부터 시작이므로 + 1, 첫 시작부분을 포함해야하므로 또 +1 ==> +2를 해주어야 한다
+                j = fail[j];                                             // 다음 일치 부분을 찾기 위해, 패턴의 인덱스를 다음 최대 일치부분으로 하여 다시 탐색한다.
 
                 answer.push_back(i - patternSize + 2);
             }
             else
+            {
+                // 실패함수 생성때와 같이, 각 인덱스의 문자열이 일치하면 패턴의 인덱스 또한 증가시켜준다.
                 j++;
+            }
+        }
         i++;
     }
 
