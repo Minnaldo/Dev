b16213170c0854c62d29c81247e346332fbb7719
AlBaneo93 <albaneo0724@gmail.com>
2019-06-14 22:29:07 +0900

    Update SWEA.cpp

--- C++/SWEA.cpp
+++ C++/SWEA.cpp
@@ -1,28 +1,30 @@
 #include <algorithm>
+#include <chrono>
 #include <iostream>
 #include <queue>
 #include <vector>
 
 using namespace std;
 
-int n, t, maxT, minT;
+int n, t, res;
 int dir[4][2] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
-vector<int> tasty;
+vector<vector<bool>> visit(100, vector<bool>(100, false));
+vector<int> tasty(100, 0);
+
 int max(int &a, int &b) { return a > b ? a : b; }
 int min(int &a, int &b) { return a < b ? a : b; }
 
 // calculate a block of cheese
-int calc(vector<vector<bool>> &cpy)
+int calc(vector<vector<int>> &cheese, int taste)
 {
     int ret = 0;
-    auto visit = cpy;
 
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
         {
             // bfs start
-            if (!visit[i][j])
+            if (cheese[i][j] > taste && !visit[i][j])
             {
                 queue<pair<int, int>> q;
                 q.push(make_pair(i, j));
@@ -39,14 +41,13 @@ int calc(vector<vector<bool>> &cpy)
 
                         if (ny >= 0 && nx >= 0 && ny < n && nx < n)
                         {
-                            if (!visit[ny][nx])
+                            if (cheese[ny][nx] > taste && !visit[ny][nx])
                             {
                                 q.push(make_pair(ny, nx));
                                 visit[ny][nx] = true;
                             }
                         }
                     }
-
                     q.pop();
                 }
                 // bfs가 끝날때 마다 1씩 증가해 덩어리수 확인
@@ -58,29 +59,33 @@ int calc(vector<vector<bool>> &cpy)
     return ret;
 }
 
-int solution(vector<vector<int>> &cheese, vector<vector<bool>> &visit)
+void dfs(vector<vector<int>> &cheese, int cury, int curx, int taste)
 {
-    int ans = 0;
-
-    // the range of taste of cheese
-    for (int a : tasty)
+    for (int i = 0; i < 4; i++)
     {
-        bool flag = false;
-        for (int i = 0; i < n; i++)
+        int ny = dir[i][0] + cury;
+        int nx = dir[i][1] + curx;
+
+        if (ny >= 0 && nx >= 0 && ny < n && nx < n)
         {
-            for (int j = 0; j < n; j++)
+            if (cheese[ny][nx] > taste && !visit[ny][nx])
             {
-                // The pairy eats the Cheese that is tasty level 'a'
-                if (cheese[i][j] == a)
-                {
-                    visit[i][j] = true;
-                    flag = true;
-                }
+                visit[ny][nx] = true;
+                dfs(cheese, ny, nx, taste);
             }
         }
+    }
+    res++;
+}
 
-        if (flag)
-            ans = max(ans, calc(visit));
+int solution(vector<vector<int>> &cheese)
+{
+    int ans = 0;
+    // the range of taste of cheese
+    for (int i = 0; i < 100; i++)
+    {
+        if (tasty[i] != 0)
+            ans = max(ans, calc(cheese, i));
     }
 
     return ans;
@@ -94,12 +99,11 @@ int main(int argc, char const *argv[])
 
     for (int tc = 1; tc <= t; tc++)
     {
+        chrono::system_clock::time_point start = chrono::system_clock::now();
+
         cin >> n;
-        maxT = 0;
-        minT = 101;
 
         vector<vector<int>> cheese(n, vector<int>(n));
-        vector<vector<bool>> visit(n, vector<bool>(n, false));
 
         for (int i = 0; i < n; i++)
         {
@@ -108,15 +112,24 @@ int main(int argc, char const *argv[])
                 int tmp;
                 cin >> tmp;
                 cheese[i][j] = tmp;
-                maxT = maxT > tmp ? maxT : tmp;
-                minT = minT < tmp ? minT : tmp;
 
-                if (find(tasty.begin(), tasty.end(), tmp) == tasty.end())
-                    tasty.push_back(tmp);
+                if (tasty[tmp] != 0)
+                    tasty[tmp]++;
             }
         }
 
-        cout << "#" << tc << " " << solution(cheese, visit) << endl;
+        // remve duplicated elements
+        // sort(tasty.begin(), tasty.end());
+        // tasty.erase(unique(tasty.begin(), tasty.end()), tasty.end());
+
+        std::cout << "#" << tc << " " << solution(cheese) << endl;
+
+        std::cout << chrono::duration<double>(chrono::system_clock::now() - start).count() << " sec" << endl;
+
+        tasty.assign(100, 0);
+        for (vector<bool> vec : visit)
+            vec.assign(100, false);
     }
+
     return 0;
 }
