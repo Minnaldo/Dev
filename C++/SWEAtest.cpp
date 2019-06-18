55991f7dd779087a17c11964340be1e96e2f41fd
AlBaneo93 <albaneo0724@gmail.com>
2019-06-18 22:34:50 +0900

    Update SWEAtest.cpp

--- C++/SWEAtest.cpp
+++ C++/SWEAtest.cpp
@@ -9,20 +9,23 @@ int n, t;
 
 string solution(vector<pair<string, int>> name)
 {
-    // for (pair<string, int> &aa : name)
     for (int i = 0; i < n; i++)
     {
         string tmp = name[i].first;
-        sort(tmp.rbegin(), tmp.rend());
-        tmp.erase(unique(tmp.begin(), tmp.end()), tmp.end());
-        name[i].second = tmp.back() != ' ' ? tmp.size() : tmp.size() - 1;
+
+        while (find(tmp.begin(), tmp.end(), ' ') != tmp.end())
+            tmp.erase(find(tmp.begin(), tmp.end(), ' '));
+        sort(tmp.begin(), tmp.end());
+
+        auto itr = unique(tmp.begin(), tmp.end());
+        name[i].second = itr - tmp.begin();
     }
 
     sort(name.begin(), name.end(), [](pair<string, int> p1, pair<string, int> p2) {
         if (p1.second > p2.second)
             return true;
         else if (p1.second == p2.second)
-            if (p1.first.compare(p2.first) <= 0)
+            if (p1.first.compare(p2.first) < 0)
                 return true;
         return false;
     });
@@ -33,18 +36,22 @@ string solution(vector<pair<string, int>> name)
 int main(int argc, char const *argv[])
 {
     freopen("input.txt", "r", stdin);
-    cin >> t;
+    scanf("%d", &t);
 
     for (int tc = 1; tc <= t; tc++)
     {
-        cin >> n;
+        scanf("%d", &n);
         vector<pair<string, int>> name;
 
         string tmp;
-        getline(cin, tmp); //  ??
+        getline(cin, tmp); //  ?? cin은 공백 또는 개행문자가 나오면 입력 받는것을 중지하는데 이 때, 공백 또는 개행문자를 버퍼에 그대로 나두기 때문에 해주어야 함
         for (int i = 0; i < n; i++)
         {
             getline(cin, tmp);
+
+            if (tmp.back() == ' ')
+                tmp.pop_back();
+
             name.push_back(make_pair(tmp, 0));
         }
 
