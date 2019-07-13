fdb723311f39a2c06a70141626f32a9cbaa25ba1
AlBaneo93 <albaneo0724@gmail.com>
2019-07-13 19:07:34 +0900

    summary that studied java

---
+++ Java/for_loop_test.java
@@ -0,0 +1,22 @@
+public class for_loop_test{
+    public static void main (String [] args)
+    {
+        int sum = 0;
+
+
+        for(int i = 0; i<9; i++)
+        {
+            sum += i;
+            System.out.println(i);
+        }
+        System.out.println(sum);
+        sum = 0;
+        System.out.println("\n\n\n");
+        for(int i = 0; i<9; ++i)
+        {
+            sum += i;
+            System.out.println(i);
+        }
+        System.out.println(sum);
+    }
+}
