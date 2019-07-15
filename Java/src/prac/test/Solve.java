a4403df4a6a5733a1c5709d47c765b8c30a811bc
AlBaneo93 <albaneo0724@gmail.com>
2019-07-16 00:09:21 +0900

    l

--- Java/src/prac/test/Solve.java
+++ Java/src/prac/test/Solve.java
@@ -1,16 +1,15 @@
-package src.prac.test;
 
 import java.util.Scanner;
 
 class Solve {
-    static int dir[][] = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+    static int dir[][] = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } }; // 우, 하, 좌, 상
     static boolean visit[][];
     static int num = 0;
     static int ans[][];
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        System.out.println("값을 입력해 주세요");
         int t = sc.nextInt();
 
         for (int tc = 1; tc <= t; tc++) {
@@ -31,7 +30,7 @@ class Solve {
         sc.close();
     }
 
-    static public void dfs(int y, int x, int cnt) {
+    public void dfs(int y, int x, int cnt) {
         for (int i = 0; i < 4; i++) {
             int ny = y + dir[i][0];
             int nx = x + dir[i][1];
@@ -39,6 +38,7 @@ class Solve {
 
             if (ny >= 0 && nx >= 0 && ny < num && nx < num) {
                 if (!visit[ny][nx]) {
+                    visit[ny][nx] = true;
                     dfs(ny, nx, cnt + 1);
                 }
             }
