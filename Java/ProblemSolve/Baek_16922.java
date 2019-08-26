8f53891f216f805b6c2dda27629dea4354aa1248
AlBan_Home <albaneo0724@gmail.com>
2019-08-27 02:07:43 +0900

    problem solve

--- Java/ProblemSolve/Baek_16922.java
+++ Java/ProblemSolve/Baek_16922.java
@@ -1,29 +1,41 @@
 import java.io.*;
+import java.util.*;
 
 public class Baek_16922 {
 
-    static int[][] arr;
+    static int n;
+    static int[] arr = { 1, 5, 10, 50 };
+    static HashSet<Integer> set;
+
+    static void recur(int idx, int sum, int num) {
+
+        if (idx == n) {
+            set.add(sum);
+            return;
+        }
+// TODO : need backtarck
+        if (num >= 50) {
+            recur(idx + 1, sum + 50, num);
+        }
+        if (num >= 10) {
+            recur(idx + 1, sum + 10, num);
+        }
+        if (num >= 5) {
+            recur(idx + 1, sum + 5, num);
+        }
+        recur(idx + 1, sum + 1, num);
+    }
 
     public static void main(String[] args) {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-            int n = Integer.parseInt(br.readLine());
+            n = Integer.parseInt(br.readLine());
             int ans = 0;
-            arr = new int[21][4];
+            // arr = new int[21][4];
+            set = new HashSet<>();
 
-            // arr[0][0] = arr[0][1] = arr[0][2] = arr[0][3] = 1;
-            arr[1][0] = arr[1][1] = arr[1][2] = arr[1][3] = 1;
-
-            for (int i = 2; i <= 20; i++) {
-                arr[i][0] = 1;
-                for (int j = 1; j < 4; j++) {
-                    arr[i][j] = arr[i][j - 1] + arr[i - 1][j];
-                }
-            }
-
-            for (int i = 0; i < 4; i++) {
-                ans += arr[n][i];
-            }
-
+            for (int i = 0; i < 4; i++)
+                recur(0, 0, arr[i]);
+            ans = set.size();
             System.out.println(ans);
 
         } catch (Exception e) {
