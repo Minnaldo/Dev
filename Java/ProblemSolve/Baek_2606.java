6733da478dab046ef55a44376d88f2fbcf1af251
AlBaneo93 <albaneo0724@gmail.com>
2019-09-07 00:26:05 +0900

    a

---
+++ Java/ProblemSolve/Baek_2606.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Baek_2606 {
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int n = Integer.parseInt(br.readLine());
+            int k = Integer.parseInt(br.readLine());
+            StringTokenizer st;
+            boolean[][] map = new boolean[n + 1][n + 1];
+            boolean[] visit = new boolean[n + 1];
+            for (int i = 0; i < k; i++) {
+                st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                map[a][b] = map[b][a] = true;
+            }
+            int ans = 0;
+
+            Queue<Integer> q = new LinkedList<>();
+            q.add(1);
+            visit[1] = true;
+            while (!q.isEmpty()) {
+                int cur = q.poll();
+
+                for (int i = 1; i <= n; i++) {
+                    if (map[cur][i] && !visit[i]) {
+                        q.add(i);
+                        visit[i] = true;
+                        map[cur][i] = map[i][cur] = false;
+                        ans++;
+                    }
+                }
+            }
+            System.out.println(ans);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
