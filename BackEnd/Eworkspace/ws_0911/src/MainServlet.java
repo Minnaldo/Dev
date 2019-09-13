26a0e6ab2c3be62ac6eda449eed0714098c3a198
AlBaneo93 <albaneo0724@gmail.com>
2019-09-13 14:48:56 +0900

    kk

--- BackEnd/Eworkspace/ws_0911/src/MainServlet.java
+++ BackEnd/Eworkspace/ws_0911/src/MainServlet.java
@@ -33,18 +33,23 @@ public class MainServlet extends HttpServlet {
 		String currency = req.getParameter("currency");
 		String desc = req.getParameter("description");
 
-		out.print("<!DOCTYPE html>" + "<html>" + "<head>"
-				+ "<meta charset='UTF-8'>"
-				+ "<title>도서 상세 정보 페이지</title>"
-				+ "<script src=\"https://code.jquery.com/jquery-3.4.1.js\"></script>" + "<script>" + "$(function(){"
-				+ "$('th').css('border','1').css('bgcolor','gray');" + "$('.index').css('bgcolor','gray');" + "});"
-				+ "</script>" + "</head>" + "<body>" + "<h1>입력된 도서 정보</h1>" + "<table>" + "<th>도서정보</th>" + "<tr>"
-				+ "<td>도서명</td><td>" + title + "</td>" + "<tr><td class='index'>도서번호</td><td>" + isbn + "-" + isbn1 + "-"
-				+ isbn2 + "</td></tr>" + "<tr><td class='index'>도서분류</td><td>" + catalogue + "</td></tr>"
-				+ "<tr><td class='index'>도서국가</td><td>" + nation + "</td></tr>" + "<tr><td class='index'>출판일</td><td>" + publisheDate
-				+ "</td></tr>" + "<tr><td class='index'>출판사</td><td>" + publisher + "</td></tr>" + "<tr><td class='index'>저 자</td><td>"
-				+ author + "</td></tr>" + "<tr><td class='index'>도서가격</td><td>" + price + currency + "</td></tr>"
-				+ "<tr><td class='index'>도서설명</td><td>" + desc + "</td>" + "</tr>" + "</table>" + "</body>" + "</html>");
+		out.print("<!DOCTYPE html>" + "<html>" + "<head>" + "<meta charset='UTF-8'>" + "<title>도서 상세 정보 페이지</title>"
+				+ "<script src=\"https://code.jquery.com/jquery-3.4.1.js\"></script>" + "<style>" + "div{"
+				+ "width : 100%;" + "margin:auto;" + "}" + "table, th, td{" + "border: 1px solid black;" + "}" + "td {"
+				+ "width : 150px;" + "height : 35px;" + "}" + ".index{" + "text-align : center;"
+				+ "background-color : rgb(213,213,213);" + "}" + "tr{" + "width : 400px;" + "height : 35px;" + "}"
+				+ "th {" + "height : 35px;" + "background-color : rgb(213,213,213);" + "}" + "table{" + "width:500px;"
+				+ "margin:auto;" + "}" + "</style>" + "</head>" + "<body>"
+				+ "<div><h1 style='text-align:center;'>입력된 도서 정보</h1></div>" + "<div style=''><table>"
+				+ "<th colspan='2'>도서정보</th>" + "<tr>" + "<td class='index'>도서명</td><td>" + title + "</td>"
+				+ "<tr><td class='index'>도서번호</td><td>" + isbn + "-" + isbn1 + "-" + isbn2 + "</td></tr>"
+				+ "<tr><td class='index'>도서분류</td><td>" + catalogue + "</td></tr>"
+				+ "<tr><td class='index'>도서국가</td><td>" + nation + "</td></tr>" + "<tr><td class='index'>출판일</td><td>"
+				+ publisheDate + "</td></tr>" + "<tr><td class='index'>출판사</td><td>" + publisher + "</td></tr>"
+				+ "<tr><td class='index'>저 자</td><td>" + author + "</td></tr>" + "<tr><td class='index'>도서가격</td><td>"
+				+ price + currency + "</td></tr>" + "<tr><td class='index'>도서설명</td><td>" + desc + "</td>" + "</tr>"
+				+ "</table></div>" + "<br><div style='text-align:center;'><a href='./book.html'>도서 등록</a></div>"
+				+ "</body>" + "</html>");
 	}
 
 }
