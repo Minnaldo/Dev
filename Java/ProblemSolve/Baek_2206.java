9a2abd099c2ff751b9701f8ce3abf845c9f943a3
AlBan_Home <albaneo0724@gmail.com>
2019-09-18 21:53:35 +0900

    jhg

--- Java/ProblemSolve/Baek_2206.java
+++ Java/ProblemSolve/Baek_2206.java
@@ -24,19 +24,47 @@ public class Baek_2206 {
         }
         visit[0][0] = true;
         Queue<Node> q = new LinkedList<>();
-        q.add(new Node(0, 0, true));
+        q.add(new Node(0, 0, 1, true));
+
+        while (!q.isEmpty()) {
+            Node node = q.poll();
+            if (node.y == n - 1 && node.x == m - 1) {
+                ans = Math.min(ans, node.cnt);
+                // continue;
+            }
+            int tmpCnt = node.cnt;
+            for (int i = 0; i < 4; i++) {
+                int ny = node.y + dir[i][0];
+                int nx = node.x + dir[i][1];
+
+                if (ny >= 0 && nx >= 0 && ny < n && nx < m) {
+                    if (!visit[ny][nx]) {
+                        if (map[ny][nx] == 0) {
+                            visit[ny][nx] = true;
+                            q.add(new Node(ny, nx, tmpCnt + 1, node.through));
+                        } else {
+                            if (node.through) {
+                                visit[ny][nx] = true;
+                                q.add(new Node(ny, nx, tmpCnt + 1, false));
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
         ans = ans == 987654321 ? -1 : ans;
         System.out.println(ans);
     }
 
     static class Node {
-        int y, x;
+        int y, x, cnt;
         boolean through;
 
-        Node(int y, int x, boolean through) {
+        Node(int y, int x, int cnt, boolean through) {
             this.y = y;
             this.x = x;
+            this.cnt = cnt;
             this.through = through;
         }
     }
