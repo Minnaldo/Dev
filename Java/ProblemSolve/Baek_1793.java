2dcf164b7782d217b288399ca6c76afb1ad879f1
AlBaneo93 <albaneo0724@gmail.com>
2019-09-25 19:52:27 +0900

    update

--- Java/ProblemSolve/Baek_1793.java
+++ Java/ProblemSolve/Baek_1793.java
@@ -2,21 +2,26 @@ import java.io.*;
 import java.math.BigInteger;
 import java.util.*;
 
+/** 타일링_백준
+ *
+ */
+
 public class Baek_1793 {
     public static void main(String[] args) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BigInteger[] memo = new BigInteger[251];
+        BigInteger[] memo = new BigInteger[252];
         memo[0] = BigInteger.ZERO;
         memo[1] = BigInteger.ONE;
-        BigInteger mul = new BigInteger("2");
-        for (int i = 2; i <= 250; i++) {
+        memo[2] = new BigInteger("3", 10);
+        BigInteger mul = new BigInteger("2", 10);
+        for (int i = 2; i <= 251; i++) {
             memo[i] = memo[i - 2].multiply(mul).add(memo[i - 1]);
         }
 
         while (true) {
             try {
                 int n = Integer.parseInt(br.readLine());
-                System.out.println(memo[n].toString(10));
+                System.out.println(memo[n + 1].toString(10));
             } catch (Exception e) {
                 return;
             }
